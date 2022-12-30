b56e3d71d5da179e070d6fcfdb5e211a5e40f0bd
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2022-12-30 17:02:49 +0530

    Time: 145 ms (81.34%), Space: 93.4 MB (53.25%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int ans=INT_MIN,dif;
+        int k=prices[0];
+        for(int i=1;i<prices.size();i++)
+        {
+            dif=prices[i]-k;
+            if(dif<0) k=prices[i];
+            else ans=max(ans,dif);
+        }
+        if(ans==INT_MIN) return 0;
+        return ans;
+        
+        
+    }
+};
