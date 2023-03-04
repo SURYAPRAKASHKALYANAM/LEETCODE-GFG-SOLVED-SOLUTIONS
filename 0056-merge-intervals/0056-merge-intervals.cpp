ea13d8a9526f2723101d5a5ab4853d0a2614f45d
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2023-03-04 16:50:32 +0530

    Time: 396 ms (5.06%), Space: 19 MB (76.54%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+        vector<vector < int>> merge(vector<vector < int>> &nums)
+        {
+            vector<vector < int>> sol;
+            sort(nums.begin(), nums.end());
+            int start, end;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                start = nums[i][0], end = nums[i][1];
+                if (!sol.empty())
+                {
+                    if (start <= sol.back()[1]) continue;
+                }
+                for (int j = i + 1; j < nums.size(); j++)
+                {
+                    if (nums[j][0] <= end) end = max(end, nums[j][1]);
+                }
+                end = max(end, nums[i][1]);
+                sol.emplace_back(std::vector<int>{start,end});
+            }
+            return sol;
+        }
+};
