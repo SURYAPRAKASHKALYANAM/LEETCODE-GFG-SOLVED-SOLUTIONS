cf43bcc0e71f2f7a4147edf01f9b0e0311f12679
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2023-05-16 09:48:35 +0530

    Time: 704 ms (30.16%), Space: 180.1 MB (54.04%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -1,38 +1,28 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode* swapNodes(ListNode* head, int k) 
-    {
-        ListNode* temp=head;
-        ListNode* temp1;
-        ListNode* temp2;
-        for(int i=0;i<k-1;i++)
+class Solution
+{
+    public:
+        ListNode* swapNodes(ListNode *head, int k)
         {
-            temp=temp->next;
+            ListNode *ptr1 = head, *ptr2 = head, *kth;
+            while(--k) ptr1 = ptr1->next;
+            kth=ptr1;
+            ptr1=ptr1->next;
+            while(ptr1)
+            {
+                ptr1=ptr1->next;
+                ptr2=ptr2->next;
+            }
+            swap(kth->val,ptr2->val);
+            return head;
         }
-        temp1=temp;
-        temp=head;
-        int c=0;
-        while(temp!=NULL)
-        {
-            c++;
-            temp=temp->next;
-        }
-        c-=k;
-        temp=head;
-        for(int i=0;i<c;i++)
-            temp=temp->next;
-        temp2=temp;
-        swap(temp1->val,temp2->val);
-        return head;
-    }
 };
