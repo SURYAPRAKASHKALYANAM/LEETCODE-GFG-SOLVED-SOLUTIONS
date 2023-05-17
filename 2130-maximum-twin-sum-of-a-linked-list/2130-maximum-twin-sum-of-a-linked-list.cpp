79dd3368154a5b10c754ce16c32fa02ae7a6e4b6
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2023-05-17 10:32:24 +0530

    Time: 293 ms (14.97%), Space: 119.5 MB (54.85%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,11 +1,11 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *  int val;
+ *  ListNode * next;
+ *  ListNode() : val(0), next(nullptr) {}
+ *  ListNode(int x) : val(x), next(nullptr) {}
+ *  ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
 class Solution
@@ -17,13 +17,14 @@ class Solution
             stack<int> s;
             while (fast && fast->next)
             {
-                s.push(slow->val);
+                s.push(slow->val);	// STORE FIRST HALF OF THE ELEMENTS IN STACK
                 slow = slow->next;
                 fast = fast->next->next;
-            }
+            }	//SLLOW POINTER REACHES MID AFTER THIS TRAVERSAL
             int maxi = 0;
-            while (slow)
+            while (slow)	// BY POPPING AND ADDING THE STACK TOP
             {
+               	//ELEMENT TO ITS TWIN PAIR IN SECOND HALF
                 maxi = max(maxi, slow->val + s.top());
                 s.pop();
                 slow = slow->next;
