c4ac3595715785085f65075a89650e562fb141ef
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2023-05-17 11:54:26 +0530

    Time: 753 ms (63.66%), Space: 252.9 MB (97.77%) - LeetHub

---
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -0,0 +1,32 @@
+/**
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
+ */
+class Solution
+{
+    public:
+        ListNode* mergeNodes(ListNode *head)
+        {
+            ListNode *temp = head, *nexti;
+            while (temp->next)
+            {
+                if (temp->val == 0)
+                {
+                    nexti = temp;
+                    temp = temp->next;
+                    continue;
+                }
+                nexti->val += temp->val;
+                nexti->next = temp->next;
+                temp = temp->next;
+            }
+            nexti->next = NULL;
+            return head;
+        }
+};
