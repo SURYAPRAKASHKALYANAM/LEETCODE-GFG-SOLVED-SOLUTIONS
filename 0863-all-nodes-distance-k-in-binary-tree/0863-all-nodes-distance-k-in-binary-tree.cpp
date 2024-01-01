5580b653661d8ee08c511b7502c6461af6299181
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-01-01 16:27:31 +0530

    Time: 12 ms (5.83%), Space: 13.5 MB (50.08%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,68 @@
+/**
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *};
+ */
+class Solution
+{
+    public:
+        void pre(TreeNode *root, map<TreeNode*, TreeNode*> &parent)
+        {
+            if (!root) return;
+            if (root->left) parent[root->left] = root;
+            if (root->right) parent[root->right] = root;
+            pre(root->left, parent);
+            pre(root->right, parent);
+        }
+    vector<int> distanceK(TreeNode *root, TreeNode *target, int k)
+    {
+       	//         find parent to each node 
+        map<TreeNode*, TreeNode*> parent;
+        pre(root, parent);
+       	//         traverse radially in all directions maintaing visited map and a queue until distance is k from target
+       	//         to move top use parent ,to go down use left and right
+        int dis = 0;
+        map<TreeNode*, bool> visited;
+        queue<TreeNode*> q;
+        q.push(target);
+        visited[target] = true;
+        while (dis < k)
+        {
+            dis++;
+            int n = q.size();
+            for (int i = 0; i < n; i++)
+            {
+                auto it = q.front();
+                q.pop();
+               	// move top
+                if (parent[it] && !visited[parent[it]])
+                {
+                    q.push(parent[it]);
+                    visited[parent[it]] = true;
+                }
+               	// move down-left,right
+                if (it->left && !visited[it->left])
+                {
+                    q.push(it->left);
+                    visited[it->left] = true;
+                }
+                if (it->right && !visited[it->right])
+                {
+                    q.push(it->right);
+                    visited[it->right] = true;
+                }
+            }
+        }
+        vector<int> ans;
+        while (!q.empty())
+        {
+            ans.emplace_back(q.front()->val);
+            q.pop();
+        }
+        return ans;
+    }
+};
