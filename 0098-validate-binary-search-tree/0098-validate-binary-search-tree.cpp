62ff0512119d7451158d5a8dffbb9103f702be62
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-01-04 00:07:26 +0530

    Time: 8 ms (70.19%), Space: 22.1 MB (31.78%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,32 +1,24 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *int val;
- *TreeNode * left;
- *TreeNode * right;
- *TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        void inorder(vector<int> &nums,TreeNode* root)
+        bool check(TreeNode *root, long low, long max)
         {
-            if(!root) return;
-            inorder(nums,root->left);
-            nums.emplace_back(root->val);
-            inorder(nums,root->right);
+            if (!root) return true;
+            return (((root->val > low) && (root->val < max)) && check(root->left, low, root->val) && check(root->right, root->val, max));
         }
-        bool isValidBST(TreeNode *root)
-        {
-            vector<int> nums;
-            inorder(nums,root);
-            for(int i=0;i<nums.size()-1;i++)
-            {
-                if(nums[i]>=nums[i+1]) return false;
-            }
-            return true;
-        }
+    bool isValidBST(TreeNode *root)
+    {
+        return check(root, LONG_MIN, LONG_MAX);
+    }
 };
