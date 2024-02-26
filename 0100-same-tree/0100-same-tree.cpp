0eac852d880a454536adfedeac2fbd1c08176d14
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-02-26 20:00:58 +0530

    Time: 0 ms (100.00%), Space: 11.7 MB (5.64%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,22 +11,9 @@
  */
 class Solution {
 public:
-    void  preorder(TreeNode *p,vector<int>& v)
-    {
-        if(p==NULL)
-        {
-            v.emplace_back(NULL);
-            return;
-        }
-        v.emplace_back(p->val);
-        preorder(p->left,v);
-        preorder(p->right,v);
-    }
-    bool isSameTree(TreeNode* p, TreeNode* q) 
-    {
-        vector<int> pv,qv;
-        preorder(p,pv);
-        preorder(q,qv);
-        return pv==qv;
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL || q == NULL) return (p == q);
+        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        
     }
 };
