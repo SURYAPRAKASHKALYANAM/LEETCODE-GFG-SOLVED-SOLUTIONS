7170786f35a09fa458df9fe59acb02848c20d5e7
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-03-28 21:54:26 +0530

    Time: 187 ms (94.00%), Space: 148.4 MB (86.57%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,24 +1,22 @@
-class Solution {
-public:
-    int maxSubarrayLength(vector<int>& nums, int k) {
-        unordered_map<int,int> count;
-        int max_len=0;
-        int left=0,right=0;
-        while(right<nums.size())
+class Solution
+{
+    public:
+        int maxSubarrayLength(vector<int> &nums, int k)
         {
-            count[nums[right]]++;
-            if(count[nums[right]]>k)
+            unordered_map<int, int> count;
+            int max_len = 0;
+            int left = 0, right = 0;
+            while (right < nums.size())
             {
-                while(nums[left]!=nums[right])
+                count[nums[right]]++;
+                if (count[nums[right]] > k)
                 {
+                    while (nums[left] != nums[right]) count[nums[left++]]--;
                     count[nums[left++]]--;
                 }
-                count[nums[left++]]--;
+                max_len = max(max_len, (right - left + 1));
+                right++;
             }
-            max_len=max(max_len,(right-left+1));
-            right++;
+            return max_len;
         }
-        return max_len;
-        
-    }
 };
