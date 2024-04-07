5de547b0313f686eb7ee728b8a0c8b7047eda9b9
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-04-07 12:56:08 +0530

    Time: 868 ms (5.53%), Space: 0B (100.00%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,5 @@
+SELECT 
+    (CASE 
+        WHEN (SELECT COUNT(DISTINCT salary) FROM Employee) < 2 THEN NULL 
+        ELSE (SELECT DISTINCT salary FROM Employee ORDER BY salary DESC LIMIT 1 OFFSET 1) 
+    END) AS SecondHighestSalary;
