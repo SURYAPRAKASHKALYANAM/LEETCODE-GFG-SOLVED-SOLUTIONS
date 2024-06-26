6db593ae6dc4149e6516a092909fe72f3ad05a79
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-06-27 05:19:50 +0530

    Time: 82 ms (75.28%), Space: 46.3 MB (97.81%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -1,35 +1,37 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    vector<TreeNode*> sorted;
-    TreeNode* balanceBST(TreeNode* root) {
-        inorder(root);
-        return merge(0,sorted.size()-1);
-    }
-    void inorder(TreeNode* root)
+class Solution
+{
+    public:
+        vector<TreeNode*> sorted;
+    TreeNode* balanceBST(TreeNode *root)
     {
-        if(!root) return;
+        inorder(root);
+        return merge(0, sorted.size() - 1);
+    }
+    void inorder(TreeNode *root)
+    {
+        if (!root) return;
         inorder(root->left);
         sorted.emplace_back(root);
         inorder(root->right);
     }
-    TreeNode* merge(int start,int end)
+    TreeNode* merge(int start, int end)
     {
-        if(start>end) return NULL;
-        int mid=(start+end)/2 ; // start + (end-start)/2;
-        TreeNode* root=sorted[mid];
-        root->left=merge(start,mid-1);
-        root->right=merge(mid+1,end);
+        if (start > end) return NULL;
+        int mid = (start + end) / 2;	// start + (end-start)/2;
+        TreeNode *root = sorted[mid];
+        root->left = merge(start, mid - 1);
+        root->right = merge(mid + 1, end);
         return root;
     }
 };
