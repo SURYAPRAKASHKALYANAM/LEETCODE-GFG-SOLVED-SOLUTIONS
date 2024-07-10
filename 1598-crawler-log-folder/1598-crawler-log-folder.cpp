31cbd7a586b38346c1dca49740822123d094af6f
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-07-10 09:57:11 +0530

    Time: 9 ms (22.38%), Space: 14 MB (61.11%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -3,7 +3,6 @@ class Solution
     public:
         int minOperations(vector<string> &logs)
         {
-           	// stack<string> s;
             int t = 0;
             for (int i = 0; i < logs.size(); i++)
             {
