065012d3c9fcdcf1d498644bb4b4be1a1c115297
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-07-11 06:40:05 +0530

    Time: 5 ms (13.35%), Space: 9 MB (26.16%) - LeetHub

---
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+    public:
+        string reverseParentheses(string s)
+        {
+            stack<char> st;
+            int begin = 0, end = s.size() - 1;
+            string ans = "", first = "", last = "", str = "";
+            for (begin = 0; begin < s.size(); begin++)
+            {
+                if (s[begin] == '(') break;
+                first += s[begin];
+            }
+            if (begin == s.size()) return first;
+            for (; end >= 0; end--)
+            {
+                if (s[end] == ')') break;
+                last = s[end] + last;
+            }
+            while (begin < end + 1)
+            {
+                if (s[begin] == ')')
+                {
+                    str = "";
+                    while (!st.empty() && st.top() != '(')
+                    {
+                        str += st.top();
+                        st.pop();
+                    }
+                    if (!st.empty()) st.pop();
+                    for (int k = 0; k < str.size(); k++) st.push(str[k]);
+                }
+                else st.push(s[begin]);
+                begin++;
+            }
+            while (!st.empty())
+            {
+                ans = st.top() + ans;
+                st.pop();
+            }
+            return first + ans + last;
+        }
+};
