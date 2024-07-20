bf1c1cc0dc43412fa7d1c748429532810ef1aa1c
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-07-20 12:50:11 +0530

    Time: 2 ms (82.47%), Space: 53.1 MB (26.69%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
@@ -2,12 +2,21 @@ class Solution {
 
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
         int[][] ans = new int[rowSum.length][colSum.length];
-        for (int i = 0; i < rowSum.length; i++) {
-            for (int j = 0; j < colSum.length; j++) {
-                ans[i][j] = Math.min(rowSum[i], colSum[j]);
-                rowSum[i] -= ans[i][j];
-                colSum[j] -= ans[i][j];
-            }
+        //O(n*m)
+        // for (int i = 0; i < rowSum.length; i++) {
+        //     for (int j = 0; j < colSum.length; j++) {
+        //         ans[i][j] = Math.min(rowSum[i], colSum[j]);
+        //         rowSum[i] -= ans[i][j];
+        //         colSum[j] -= ans[i][j];
+        //     }
+        // }
+        // o(n+m)
+        for (int i = 0, j = 0; i < rowSum.length && j < colSum.length;) {
+            ans[i][j] = Math.min(rowSum[i], colSum[j]);
+            rowSum[i] -= ans[i][j];
+            colSum[j] -= ans[i][j];
+            i += (rowSum[i] == 0)?1:0;
+            j += (colSum[j] == 0)?1:0;
         }
         return ans;
     }
