07de940dcf3489d06fe4bff844ac9ccc9f3ee9b5
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-10-08 18:27:20 +0530

    Time: 99 ms (77.02%), Space: 32.7 MB (79.33%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,20 +1,22 @@
-class Solution {
-public:
-    int minSwaps(string s) {
-        int open=0,close=0,start=0,end=s.size()-1,swaps=0;
-        while(start<end)
+class Solution
+{
+    public:
+        int minSwaps(string s)
         {
-            if(s[start]=='[') open++;
-            else close++;
-            if(close>open)
+            int open = 0, close = 0, start = 0, end = s.size() - 1, swaps = 0;
+            while (start < end)
             {
-                close--,open++;
-                while(end>start && s[end]!='[') end--;
-                swap(s[start],s[end--]);
-                swaps++;
+                if (s[start] == '[') open++;
+                else close++;
+                if (close > open)
+                {
+                    close--, open++;
+                    while (end > start && s[end] != '[') end--;
+                    swap(s[start], s[end--]);
+                    swaps++;
+                }
+                start++;
             }
-            start++;
+            return swaps;
         }
-        return swaps;
-    }
 };
