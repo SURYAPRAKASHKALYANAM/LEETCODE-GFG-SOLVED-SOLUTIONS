03eba7bd8995416a970b5097b301bea8d1d79a38
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-10-09 07:00:41 +0530

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,18 +1,32 @@
+// class Solution
+// {
+//     public:
+//         int minAddToMakeValid(string s)
+//         {
+//             stack<char> st;
+//             for (auto i: s)
+//             {
+//                 if (i == ')')
+//                 {
+//                     if (!st.empty() && st.top() == '(') st.pop();
+//                     else st.push(i);
+//                 }
+//                 else st.push(i);
+//             }
+//             return st.size();
+//         }
+// };
 class Solution
 {
     public:
         int minAddToMakeValid(string s)
         {
-            stack<char> st;
-            for (auto i: s)
+            int cnt = 0,leftover=0;
+            for (int i = 0; i < s.size(); i++)
             {
-                if (i == ')')
-                {
-                    if (!st.empty() && st.top() == '(') st.pop();
-                    else st.push(i);
-                }
-                else st.push(i);
+                if (s[i] == '(') cnt++;
+                else cnt>0?cnt--:leftover++;
             }
-            return st.size();
+            return leftover+cnt;
         }
 };
