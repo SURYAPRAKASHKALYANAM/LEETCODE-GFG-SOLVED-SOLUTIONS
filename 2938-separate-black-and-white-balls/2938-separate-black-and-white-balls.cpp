11c19e345ea1eb9d26205bc783a99837abddea19
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-10-15 20:01:25 +0530

    Time: 28 ms (85.68%), Space: 15.5 MB (89.89%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -3,22 +3,33 @@ class Solution
     public:
         long long minimumSteps(string s)
         {
-            int black = 0, white = 0, len = s.size();
-            long long ans = 0;
-            while (white < len)
-            {
-                while (white < len && s[white] == '1') white++;
-                while (black < len && s[black] == '0') black++;
-               	// cout<<white<<" "<<black<<endl;
-                if (white < s.size() && white > black)
-                {
-                    ans += (white - black);
-                    swap(s[white], s[black]);
-                    white++, black++;
-                }
-                else white++;
-            }
+            // int black = 0, white = 0, len = s.size();
+            // long long ans = 0;
+            // while (white < len)
+            // {
+            //     while (white < len && s[white] == '1') white++;
+            //     while (black < len && s[black] == '0') black++;
+            //    	// cout<<white<<" "<<black<<endl;
+            //     if (white < s.size() && white > black)
+            //     {
+            //         ans += (white - black);
+            //         swap(s[white], s[black]);
+            //         white++, black++;
+            //     }
+            //     else white++;
+            // }
            	// cout<<s<<endl;
-            return ans;
+            // return ans;
+            // OPTIMAL APPROACH 
+            long long res = 0, cur = 0;
+            for (int i = 0; i < s.length(); ++i)
+            {
+                if (s[i] == '0')
+                {
+                    res += i - cur;
+                    ++cur;
+                }
+            }
+            return res;
         }
 };
