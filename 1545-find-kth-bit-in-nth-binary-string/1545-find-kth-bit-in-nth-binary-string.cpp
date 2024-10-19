148c87bbaa6c6ab86764d7d470cc8a8cb4526c63
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-10-19 12:19:44 +0530

    Time: 63 ms (22.36%), Space: 161.1 MB (6.15%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,17 +1,33 @@
-class Solution {
-public:
+class Solution
+{
+    public:
+    // Brute force
+    // string reversee(string s)
+    // {
+    //     for(auto &i:s) i=(i=='1')?'0':'1';
+    //     reverse(s.begin(),s.end());
+    //     return s;
+    // }
+    //=============================================================
+    // recursion
     string reversee(string s)
     {
-        for(auto &i:s) i=(i=='1')?'0':'1';
-        reverse(s.begin(),s.end());
+        for (auto &i: s) i = (i == '1') ? '0' : '1';
+        reverse(s.begin(), s.end());
         return s;
     }
-    char findKthBit(int n, int k) {
-        string s="0";
-        while(n--)
-        {
-            s+="1"+reversee(s);
-        }
-        return s[k-1];
+    string find(int n, int curr, string s)
+    {
+        if (curr == n) return s;
+        s += "1" + reversee(s);
+        return find(n, curr + 1, s);
+    }
+    char findKthBit(int n, int k)
+    {
+        string s = "0";
+       	//Brute force
+       	// while(n--)  s+="1"+reversee(s);
+        s = find(n, 0, s);
+        return s[k - 1];
     }
 };
