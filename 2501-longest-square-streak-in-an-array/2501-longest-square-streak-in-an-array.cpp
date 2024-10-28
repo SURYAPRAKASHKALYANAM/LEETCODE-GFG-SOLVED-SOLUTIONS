12f0a5f8d40a996ad687f8ee306c575050898f6c
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-10-28 22:20:40 +0530

    Time: 483 ms (6.67%), Space: 161.1 MB (6.43%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.cpp
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.cpp
@@ -3,7 +3,7 @@ class Solution
     public:
         int longestSquareStreak(vector<int> &nums)
         {
-            map<long long, int> visited, have;
+            map < long long, int> visited, have;
             sort(nums.begin(), nums.end());
             int max_len = -1;
             for (int i: nums) have[i] = 1;
@@ -23,5 +23,17 @@ class Solution
                 }
             }
             return max_len;
+            //OPTIMAL DP
+           	//     map<int, int>mp;
+           	// sort(nums.begin(), nums.end());
+           	// int res = -1;
+           	// for(int num: nums) {
+           	//     int _sqrt = sqrt(num);
+           	//     if(_sqrt *_sqrt == num && mp.find(_sqrt)!=mp.end()) {
+           	//         mp[num] = mp[_sqrt]+1;
+           	//         res = max(res, mp[num]);
+           	//     } else mp[num] = 1;
+           	// }
+           	// return res;
         }
 };
