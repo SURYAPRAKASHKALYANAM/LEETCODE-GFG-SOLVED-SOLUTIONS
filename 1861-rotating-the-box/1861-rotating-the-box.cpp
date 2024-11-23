fa31bfd96de6d668ffbc960fff9e9a00e9ad44d9
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2024-11-23 20:23:46 +0530

    Time: 189 ms (69.58%), Space: 56.4 MB (25.37%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m=box.size(),n=box[0].size();
+        vector<vector<char>> ans(n,vector<char> (m,'.'));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++) ans[i][j]=box[m-1-j][i];
+        }
+        for(int i=0;i<m;i++)
+        {
+            int end=n-1,start=end;
+            while(start>=0)
+            {
+                if(ans[start][i]=='*') end=start;
+                else if(ans[start][i]=='#')
+                {
+                    while(end>=0 && (ans[end][i]=='#' || ans[end][i]=='*')) end--;
+                    if(end>=0 && end>start) ans[start][i]='.',ans[end][i]='#';
+                }
+                start--;
+            }
+        }
+        return ans;
+    }
+};
