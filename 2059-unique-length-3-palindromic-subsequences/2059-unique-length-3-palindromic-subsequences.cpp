63a408e1bd3c6306cd2d5e1d864c4616089fd4a8
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-01-04 09:51:14 +0530

    Time: 0 ms (100%), Space: 13.9 MB (96.63%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -3,22 +3,42 @@ class Solution
     public:
         int countPalindromicSubsequence(string s)
         {
-            map<char, int> right, left;
-            for (auto i: s)
-                right[i]++;
-            int cnt = 0;
-            unordered_map<string, int> m;
-            for (int i = 0; i < s.size() - 1; i++)
+           	// map<char, int> right, left;
+           	// for (auto i: s)
+           	//     right[i]++;
+           	// int cnt = 0;
+           	// unordered_map<string, int> m;
+           	// for (int i = 0; i < s.size() - 1; i++)
+           	// {
+           	//     right[s[i]]--;
+           	//     for (char j = 'a'; j <= 'z'; j++)
+           	//     {
+           	//         string st = "";
+           	//         st += j, st += s[i], st += j;
+           	//         if (left[j] && right[j] && !m[st]) m[st] = ++cnt;
+           	//     }
+           	//     left[s[i]]++;
+           	// }
+           	// return cnt;
+
+            bitset<26> exist;
+            int ans = 0;
+            for (int i = 0; i < 26; i++)
             {
-                right[s[i]]--;
-                for (char j = 'a'; j <= 'z'; j++)
+                int l = s.find('a' + i);
+                if (l != string::npos)
                 {
-                    string st = "";
-                    st += j, st += s[i], st += j;
-                    if (left[j] && right[j] && !m[st]) m[st] = ++cnt;
+                    int r = s.find_last_of('a' + i);
+                    if (r - l < 2) continue;
+                    for (int k = l + 1; k < r; k++)
+                    {
+                        exist.set(s[k] - 'a');
+                        if (exist.count() == 26) break;
+                    }
+                    ans += exist.count();
+                    exist.reset();
                 }
-                left[s[i]]++;
             }
-            return cnt;
+            return ans;
         }
 };
