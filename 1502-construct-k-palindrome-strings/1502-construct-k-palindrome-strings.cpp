bcff2164a42eb878b1273714d211c6b4d15dbdab
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-01-11 22:40:40 +0530

    Time: 5 ms (55.59%), Space: 14.8 MB (66.78%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -6,8 +6,8 @@ class Solution
             vector<int> counts(26, 0);
             int len = s.size();
             for (auto i: s) counts[i - 'a']++;
-            int odd = 0, even = 0;
-            for (int i: counts)(i & 1) ? odd++ : even++;
+            int odd = 0;
+            for (int i: counts) odd += (i & 1);
             return odd <= k && len >= k;
         }
 };
