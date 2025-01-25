bc5ffee98ca82ea236d46d0c227bd616794e8764
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-01-25 22:54:38 +0530

    Time: 10 ms (87.27%), Space: 36.1 MB (58.58%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& isWater) {
+        int m = isWater.size(), n = isWater[0].size();
+        vector<vector<int>> height(m, vector<int>(n, 0));
+        queue<pair<int, pair<int, int>>> q;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!isWater[i][j])
+                    q.push({0, {i, j}});
+            }
+        }
+        int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            int dist = it.first, r = it.second.first, c = it.second.second;
+            for (int i = 0; i < 4; i++) {
+                int row = r + dx[i], col = c + dy[i];
+                if (row >= 0 && row < m && col >= 0 && col < n) {
+                    if ((dist + 1 < height[row][col] || !height[row][col]) &&
+                        isWater[row][col]) {
+                        height[row][col] = dist + 1;
+                        q.push({dist + 1, {row, col}});
+                    }
+                }
+            }
+        }
+        return height;
+    }
+};
