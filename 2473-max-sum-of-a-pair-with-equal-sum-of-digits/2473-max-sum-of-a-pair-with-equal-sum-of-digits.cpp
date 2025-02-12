b9fc213c3cdfd3f03b8080724e89c9ff9184c360
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-02-12 05:47:00 +0530

    Time: 35 ms (42.73%), Space: 64 MB (52.02%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -9,18 +9,30 @@ public:
         return sum;
     }
     int maximumSum(vector<int>& nums) {
-        map<int, priority_queue<int>> m;
-        int ans = -1;
-        for (auto i : nums)
-            m[digitSum(i)].push(i);
-        for (auto ele : m) {
-            auto q = ele.second;
-            if (q.size() >= 2) {
-                int curr = q.top();
-                q.pop();
-                ans = max(ans, curr + q.top());
-            }
+        int maxi = -1;
+        map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            int ind = digitSum(nums[i]);
+            if (m[ind]) {
+                maxi = max(maxi, m[ind] + nums[i]);
+                m[ind] = max(m[ind], nums[i]);
+            } else
+                m[ind] = nums[i];
         }
-        return ans;
+        return maxi;
+
+        // unordered_map<int, priority_queue<int>> m;
+        // int ans = -1;
+        // for (auto i : nums)
+        //     m[digitSum(i)].push(i);
+        // for (auto ele : m) {
+        //     auto q = ele.second;
+        //     if (q.size() >= 2) {
+        //         int curr = q.top();
+        //         q.pop();
+        //         ans = max(ans, curr + q.top());
+        //     }
+        // }
+        // return ans;
     }
 };
