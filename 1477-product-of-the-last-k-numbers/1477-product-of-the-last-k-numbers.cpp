df06a1d10d5e71cac45d2f86ded7d338a9e27a8b
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-02-14 08:06:42 +0530

    Time: 13 ms (89.18%), Space: 78.1 MB (56.86%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,12 +1,30 @@
 class ProductOfNumbers {
 public:
     vector<int> q;
+    int prod = 1;
     ProductOfNumbers() {}
 
-    void add(int num) { q.push_back(num); }
+    void add(int num) {
+        // BRUTE
+        //  q.push_back(num);
+        // Optimal
+        if (num == 0) {
+            q.clear();
+            prod = 1;
+        } else
+            q.push_back(prod *= num);
+    }
 
     int getProduct(int k) {
-        return accumulate(q.end() - k, q.end(), 1, multiplies<int>());
+        // Brute
+        // return accumulate(q.end() - k, q.end(), 1, multiplies<int>());
+        // Optimal
+        if (q.size() < k)
+            return 0;
+        else if (q.size() == k)
+            return prod;
+        else
+            return prod / q[q.size() - k - 1];
     }
 };
 
