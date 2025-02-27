09e20a81fbc3f63347ca64d467944bf5e27ce3b5
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-02-28 00:58:29 +0530

    Time: 0 ms (100%), Space: 21.6 MB (35.04%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,33 +1,24 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *int val;
- *TreeNode * left;
- *TreeNode * right;
- *TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        bool add(TreeNode *root, int target, int sum)
-        {
-            if (root == NULL || (root->left == NULL && root->right == NULL)) return sum == target;
-            else if (root->left == NULL)
-            {
-                return add(root->right, target, sum + root->right->val);
-            }
-            else if (root->right == NULL)
-            {
-                return add(root->left, target, sum + root->left->val);
-            }
-            else return add(root->left, target, sum + (root->left->val)) || add(root->right, target, sum + root->right->val);
-        }
-    bool hasPathSum(TreeNode *root, int targetSum)
-    {
-        if (!root) return false;
-        return add(root, targetSum,root->val);
+class Solution {
+public:
+    bool isleaf(TreeNode* root) { return !root->left && !root->right; }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root)
+            return false;
+        if (root && isleaf(root))
+            return targetSum - root->val == 0;
+        return hasPathSum(root->left, targetSum - root->val) ||
+               hasPathSum(root->right, targetSum - root->val);
     }
 };
