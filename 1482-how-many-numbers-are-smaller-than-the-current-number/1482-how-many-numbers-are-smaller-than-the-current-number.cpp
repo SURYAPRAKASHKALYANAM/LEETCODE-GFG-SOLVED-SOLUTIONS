4fd398d088ed3d11974b251a46012f9568f9965b
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-02-28 08:55:18 +0530

    Time: 0 ms (100%), Space: 14.4 MB (41.66%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        int len = nums.size();
-        unordered_map<int, int> sorted_positions;
-        vector<int> sortednums(nums), ans(len);
-        sort(sortednums.begin(), sortednums.end());
-        for (int i = 0; i < len; i++) {
-            if (sorted_positions.find(sortednums[i]) ==
-                sorted_positions.end()) {
-                sorted_positions[sortednums[i]] = i;
-            }
-        }
-        for (int i = 0; i < len; i++) {
-            ans[i] = sorted_positions[nums[i]];
-        }
+        vector<int> counts(102, 0);
+        for (auto i : nums)
+            counts[i + 1] += 1;
+        for (int i = 1; i < 102; i++)
+            counts[i] += counts[i - 1];
+        vector<int> ans;
+        for (auto i : nums)
+            ans.emplace_back(counts[i]);
         return ans;
     }
 };
