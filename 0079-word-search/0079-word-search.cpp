9ee122058558c4d6f964e6120602cc25ef0257ae
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-03-01 04:29:44 +0530

    Time: 325 ms (57.02%), Space: 11.5 MB (48.09%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int dx[4] = {0, -1, 0, 1}, dy[4] = {-1, 0, 1, 0};
     bool dfs(int r, int c, int pos, int n, int m, int len,
-             vector<vector<char>>& board, string word,
-             vector<vector<int>> visited) {
-        if (pos+1> len)
-            return false;
-        if (word[pos] != board[r][c])
-            return false;
-        if(pos==len-1)  return true;
-        visited[r][c] = 1;
+             vector<vector<char>>& board, string word) {
+        if (pos>=len) return true;
+        char ch=board[r][c];
+        board[r][c]='#';
         for (int i = 0; i < 4; i++) {
             int row = r + dx[i], col = c + dy[i];
             if (row >= 0 && row < m && col >= 0 && col < n &&
-                !visited[row][col] && word[pos+1]==board[row][col]) {
-                if (dfs(row, col, pos + 1, n, m, len, board, word, visited))
+                word[pos]==board[row][col]) {
+                if (dfs(row, col, pos + 1, n, m, len, board, word))
                     return true;
             }
         }
+        board[r][c]=ch;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
@@ -26,8 +23,7 @@ public:
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == word[0]) {
-                    vector<vector<int>> visited(m, vector<int>(n, 0));
-                    if (dfs(i, j, 0, n, m, len, board, word, visited))
+                    if (dfs(i, j, 1, n, m, len, board, word))
                         return true;
                 }
             }
