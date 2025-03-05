05b3c03f78847784692849427038be95dbecb9f5
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-03-05 06:04:49 +0530

    Time: 0 ms (100%), Space: 8 MB (31.17%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long total = 1, m = 1;
-        for (int i = 1; i < n; i++) {
-            total += (2 * m) + 2;
-            m += 2;
-        }
-        return total;
+        long long _n = n;
+        return 1 + 2 * _n * (_n - 1);
+        // long long total = 1, m = 1;
+        // for (int i = 1; i < n; i++) {
+        //     total += (2 * m) + 2;
+        //     m += 2;
+        // }
+        // return total;
     }
 };
