ac59b8155dad9e53b3a130381bafc332ad7890ae
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-03-05 05:38:24 +0530

    Time: 0 ms (100%), Space: 28 MB (50.76%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -6,23 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-     int maxi=INT_MIN;
-    int maxii(TreeNode* root)
-    {
-        if(root==NULL) return 0;
-        int lh=max(0,maxii(root->left));
-        int rh=max(0,maxii(root->right));
-        maxi=max(maxi,(root->val)+lh+rh);
-        return (root->val)+max(lh,rh);
+    int helper(TreeNode* root, int& max_path_sum) {
+        if (!root)
+            return 0;
+        int lh = max(0, helper(root->left, max_path_sum));
+        int rh = max(0, helper(root->right, max_path_sum));
+        max_path_sum = max(max_path_sum, root->val + lh + rh);
+        return root->val + max(lh, rh);
     }
-    int maxPathSum(TreeNode* root) 
-    {
-       int s=maxii(root);
-       return maxi;
+    int maxPathSum(TreeNode* root) {
+        int max_path_sum = INT_MIN;
+        helper(root, max_path_sum);
+        return max_path_sum;
     }
 };
