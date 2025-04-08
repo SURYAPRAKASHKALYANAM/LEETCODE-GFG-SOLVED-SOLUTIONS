82244d9290d55dbb9ec13ef59ce1324da650fd3c
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-04-08 21:54:27 +0530

    Time: 3 ms (98.25%), Space: 75.9 MB (17.49%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -2,14 +2,26 @@ class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
         int minops = 0, n = nums.size();
-        sort(begin(nums), end(nums));
-        int prev = nums[0];
-        for (int i = 1; i < n; i++) {
-            if(nums[i]<=prev){
-                minops+=(prev+1-nums[i]);
-                nums[i]=prev+1;
+        // O (nlogn)
+        // sort(begin(nums), end(nums));
+        // int prev = nums[0];
+        // for (int i = 1; i < n; i++) {
+        //     if(nums[i]<=prev){
+        //         minops+=(prev+1-nums[i]);
+        //         nums[i]=prev+1;
+        //     }
+        //     prev=nums[i];
+        // }
+        // O(N)
+        vector<int> counts(n + *max_element(begin(nums), end(nums)));
+        for (auto i : nums)
+            counts[i]++;
+        for (int i = 0; i < counts.size(); i++) {
+            int remdup = counts[i] - 1;
+            if (remdup > 0) {
+                counts[i + 1] += remdup;
+                minops += remdup;
             }
-            prev=nums[i];
         }
         return minops;
     }
