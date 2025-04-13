a734798c434d56b84fae28c380a5f1eab8670033
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-04-13 10:30:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (69.99%) - LeetHub

---
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    long long power(long long x, long exp) {
+        long long ans = 1;
+        long pow = exp;
+        while (pow > 0) {
+            if (pow & 1) {
+                ans = (ans * x) % mod;
+                pow = pow - 1;
+            } else {
+                x = (x * x) % mod;
+                pow = pow / 2;
+            }
+        }
+        return ans;
+    }
+    int countGoodNumbers(long long n) {
+        int ans = 1;
+        long long even = n / 2 + (n % 2), odd = n / 2;
+        return (int)((power(5, even) * power(4, odd)) % mod);
+    }
+};
