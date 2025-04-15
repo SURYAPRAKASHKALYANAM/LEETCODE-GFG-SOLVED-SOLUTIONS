e509c93d525054fc5cfb9c1e8601ad7942aaeb0b
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-04-16 05:17:42 +0530

    Time: 2 ms (23.02%), Space: 14.9 MB (21.6%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<int> vis(rooms.size(), 0);
+        queue<int> q;
+        q.push(0);
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            if (vis[it])
+                continue;
+            vis[it] = 1;
+            for (auto neigh : rooms[it]) {
+                if (!vis[neigh]) {
+                    q.push(neigh);
+                }
+            }
+        }
+        return accumulate(begin(vis), end(vis), 0) == rooms.size();
+    }
+};
