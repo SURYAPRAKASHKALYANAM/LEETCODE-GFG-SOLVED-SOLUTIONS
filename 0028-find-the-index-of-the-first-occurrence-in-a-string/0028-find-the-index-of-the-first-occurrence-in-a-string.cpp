9ff907b83ddc48a6744506fbb0cdee6bab6e7d55
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-04-18 07:54:17 +0530

    Time: 27 ms (2.29%), Space: 8.9 MB (13.2%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        // KMP ALGO O(N)
+        int needlelen = needle.size();
+        vector<int> lps(needlelen);
+        lps[0] = 0;
+        int l = 0, i = 1;
+        while (i < needlelen) {
+            if (needle[i] == needle[l]) {
+                lps[i] = ++l;
+                i += 1;
+            } else {
+                if (l != 0) {
+                    l = lps[l - 1];
+                } else
+                    i++;
+            }
+        }
+        l = 0, i = 0;
+        while (i < haystack.size()) {
+            if (needle[l] == haystack[i]) {
+                i++;
+                l++;
+            } else {
+                if (l != 0) {
+                    l = lps[l - 1];
+                } else
+                    i++;
+            }
+            cout << l << " " << i << "\n";
+            if (l == needlelen)
+                return i - needlelen;
+        }
+        return -1;
+    }
+};
