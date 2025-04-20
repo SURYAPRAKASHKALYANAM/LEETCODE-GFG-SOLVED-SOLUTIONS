9ca13b8c5e448177c11a087ecaccf9552b3e88d0
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-04-20 06:40:35 +0530

    Time: 0 ms (100%), Space: 12.3 MB (42.46%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int numRabbits(vector<int>& answers) {
-        vector<int> counts(10e3, 0);
-        // unordered_map<int,int> counts
+        // vector<int> counts(10e3, 0);
+        unordered_map<int, int> counts;
         for (auto rabits : answers)
             counts[rabits]++;
-        int cnt = counts[0];
-        for (int i = 1; i <= 1000; i++) {
-            if (counts[i]) {
-                cnt += (counts[i] / (i + 1)) * (i + 1) + // seen each other 
-                       ((counts[i] % (i + 1) > 0 ? 1 : 0) * i + // seen different 
-                        (counts[i] % (i + 1) > 0)); // self
+        int min_rab = counts[0];
+        for (auto i : counts) {
+            int said = i.first, cnt = i.second;
+            if (said) {
+                min_rab += (cnt / (said + 1)) * (said + 1) +        // seen each other
+                           ((cnt % (said + 1) > 0 ? 1 : 0) * said + // seen different
+                           (cnt % (said + 1) > 0));                // self
             }
         }
-        return cnt;
+        // for (int i = 1; i <= 1000; i++) {
+        //     if (counts[i]) {
+        //         cnt += (counts[i] / (i + 1)) * (i + 1) + // seen each other
+        //                ((counts[i] % (i + 1) > 0 ? 1 : 0) * i + // seen
+        //                different
+        //                 (counts[i] % (i + 1) > 0)); // self
+        //     }
+        // }
+        return min_rab;
     }
 };
