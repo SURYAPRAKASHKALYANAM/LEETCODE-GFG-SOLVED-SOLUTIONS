48bfd5b3357a17e610eeacaa5bf211c8f352cda3
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-04-27 12:19:40 +0530

    Time: 9 ms (94.15%), Space: 73.2 MB (93.38%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -3,7 +3,8 @@ public:
     int totalFruit(vector<int>& fruits) {
         int start = 0, end = 0, n = fruits.size();
         int curr = 0, max_fruits = 0;
-        unordered_map<int, int> seen;
+        // unordered_map<int, int> seen;
+        vector<int> seen(n,0);
         while (end < n) {
             while (curr <= 2 && end < n) {
                 if (curr == 2 && !seen[fruits[end]]) {
