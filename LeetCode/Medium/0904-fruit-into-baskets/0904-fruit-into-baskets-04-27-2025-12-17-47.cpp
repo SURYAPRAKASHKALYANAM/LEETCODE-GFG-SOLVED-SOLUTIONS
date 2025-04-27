39c3ee2bd8217ecb96066256490200a532759b11
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-04-27 12:17:47 +0530

    59 ms (8.19%) 04-27-2025 0904-fruit-into-baskets .cpp

---
+++ LeetCode/Medium/0904-fruit-into-baskets/0904-fruit-into-baskets-04-27-2025-12-17-47.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int start = 0, end = 0, n = fruits.size();
+        int curr = 0, max_fruits = 0;
+        unordered_map<int, int> seen;
+        while (end < n) {
+            while (curr <= 2 && end < n) {
+                if (curr == 2 && !seen[fruits[end]]) {
+                    break;
+                }
+                else if (!seen[fruits[end]])
+                    curr++;
+                seen[fruits[end]]++;
+                end++;
+            }
+            max_fruits = max(max_fruits, end - start);
+            while (curr == 2 && start < end) {
+                --seen[fruits[start]];
+                if (seen[fruits[start]] == 0)
+                    curr--;
+                start++;
+            }
+        }
+        return max_fruits;
+    }
+};
