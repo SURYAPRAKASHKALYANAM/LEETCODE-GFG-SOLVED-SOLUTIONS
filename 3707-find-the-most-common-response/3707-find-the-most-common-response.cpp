96e8122040383cb260c459fc0917e9af19028344
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-05-14 20:21:57 +0530

    Time: 1332 ms (43.93%), Space: 517.2 MB (29.84%) - LeetHub

--- 3707-find-the-most-common-response/3707-find-the-most-common-response.cpp
+++ 3707-find-the-most-common-response/3707-find-the-most-common-response.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     string findCommonResponse(vector<vector<string>>& responses) {
         int n = responses.size();
-        map<string, int> counts;
+        int max_cnt = 0;
+        unordered_map<string, int> counts;
+        string ans = "";
         for (int i = 0; i < n; i++) {
             unordered_map<string, bool> seen;
             for (auto i : responses[i]) {
                 if (!seen[i]) {
                     counts[i]++;
+                    if (counts[i] >= max_cnt) {
+                        if (counts[i] == max_cnt) {
+                            ans = min(ans, i);
+                        } else
+                            ans = i;
+                        max_cnt=counts[i];
+                    }
                     seen[i] = true;
                 }
-            }
-        }
-        string ans = "";
-        int max_cnt = 0;
-        for (auto i : counts) {
-            if (i.second > max_cnt) {
-                ans = i.first;
-                max_cnt = i.second;
             }
         }
         return ans;
