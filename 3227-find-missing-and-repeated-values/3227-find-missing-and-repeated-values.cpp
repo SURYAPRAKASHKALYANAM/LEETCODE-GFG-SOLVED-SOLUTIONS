a685a0afb490c91c243f521e857e6b78e1a2d468
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-07-09 08:54:46 +0530

    Time: 0 ms (100%), Space: 25.6 MB (91.09%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n = grid.size(), _n = n * n;
-        vector<int> seen(_n + 1);
-        int tot_sum = 0, a, actual_sum = _n * (_n + 1) / 2, curr = 1;
+        long n = grid.size(), _n = n * n;
+        long tot_sum = 0, a, sq_sum = 0, actual_sum = _n * (_n + 1) / 2;
+        long aactual_sq_sum = (_n * (_n + 1) * (2 * _n + 1)) / 6;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 tot_sum += grid[i][j];
-                if (seen[grid[i][j]])
-                    a = grid[i][j];
-                seen[grid[i][j]] = 1;
+                sq_sum += grid[i][j] * grid[i][j];
             }
         }
-        return {a, actual_sum - tot_sum + a};
+        // X-Y == S - SN
+        int val1 = tot_sum - actual_sum;
+        // X2-Y2 = S2 - S2N
+        // (X-Y) * (X+Y) = S2-S2N
+        // (X+Y) = (S2-SN) / val1;
+        int val2 = (sq_sum - aactual_sq_sum) / val1;
+        int X = (val1 + val2) / 2;
+        int Y = val2 - X;
+        return {X, Y};
     }
 };
