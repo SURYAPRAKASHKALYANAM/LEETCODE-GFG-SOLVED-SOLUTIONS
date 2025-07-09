f75e057f9615a81daf4923d4f5ddf080fd6c499e
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-07-09 23:20:39 +0530

    Time: 0 ms (100%), Space: 25.6 MB (85.27%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -20,4 +20,33 @@ public:
         int Y = val2 - X;
         return {X, Y};
     }
+
+    // this is for arr-1d of 1-n
+    vector<int> findMissingRepeatingNumbers(vector<int> nums) {
+        // find xor of ( 1 to len and arr[i])
+        int len = nums.size();
+        int xor_val = 0;
+        for (int i = 0; i < len; i++) {
+            xor_val ^= nums[i];
+            xor_val ^= (i + 1);
+        }
+        int right_most = xor_val & ~(xor_val-1);
+        int Zero_xor = 0, one_xor = 0;
+        for (int i = 0; i < len; i++) {
+            if ((i + 1) & right_most)
+                one_xor ^= (i + 1);
+            else
+                Zero_xor ^= (i + 1);
+            if (nums[i] & right_most)
+                one_xor ^= nums[i];
+            else
+                Zero_xor ^= nums[i];
+        }
+        for (int i : nums) {
+            if (one_xor == i) {
+                return {one_xor, Zero_xor};
+            }
+        }
+        return {Zero_xor, one_xor};
+    }
 };
