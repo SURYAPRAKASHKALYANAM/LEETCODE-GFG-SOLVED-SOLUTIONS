47f28e66e19c438ab6d9a58692ac7eee02d1979b
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-07-16 22:37:52 +0530

    Time: 0 ms (100%), Space: 17.8 MB (56.49%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,34 +1,18 @@
-class Solution
-{
-    public:
-        int maxProduct(vector<int> &nums)
-        {
-            int leftmax=INT_MIN,rightmax=INT_MIN,prod=1;
-            bool zero=0;
-            for(auto i:nums)
-            {
-                prod*=i;
-                if(!i)
-                {
-                    prod=1;
-                    zero=1;
-                    continue;
-                }
-                leftmax=max(prod,leftmax);
-            }
-            prod=1;
-            for(int j=nums.size()-1;j>=0;j--)
-            {
-                prod*=nums[j];
-                if(!nums[j])
-                {
-                    prod=1;
-                    zero=1;
-                    continue;
-                }
-                rightmax=max(prod,rightmax);
-            }
-            if(zero) return max(0,max(leftmax,rightmax));
-            return max(leftmax,rightmax);
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int max_product = INT_MIN;
+        int pref = 1, suff = 1;
+        int len = nums.size();
+        for (int i = 0; i < len; i++) {
+            if (pref == 0)
+                pref = 1;
+            if (suff == 0)
+                suff = 1;
+            pref = pref * nums[i];
+            suff = suff * nums[len - i - 1];
+            max_product = max(max_product, max(pref, suff));
         }
+        return max_product;
+    }
 };
