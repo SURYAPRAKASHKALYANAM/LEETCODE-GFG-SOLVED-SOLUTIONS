449029d9d1aa4af17ad30f06960db69726ba3e2f
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-07-21 23:47:22 +0530

    Time: 630 ms (5.07%), Space: 246.4 MB (14.86%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -3,25 +3,23 @@ public:
     int merge(int start, int end, vector<int>& nums) {
         int cnt = 0;
         if (start < end) {
-            int mid = (start + end) / 2;
+            int mid = (end + start) / 2;
             cnt += merge(start, mid, nums);
             cnt += merge(mid + 1, end, nums);
             cnt += countPairs(start, mid, end, nums);
-            mergeSort(start, mid, end, nums);
+            mergeSort(start, mid,end, nums);
         }
         return cnt;
     }
-
     int countPairs(int start, int mid, int end, vector<int>& nums) {
-        int cnt = 0, right = mid + 1;
-        for (int i = start; i <= mid; i++) {
-            while (right <= end && ((long long)nums[i] > (long long)(2LL * nums[right])))
-                right++;
-            cnt += (right - (mid + 1));
+        int left = start, right = mid + 1, cnt = 0;
+        while(left<=mid && right<=end){
+            while(left<=mid && (long long)nums[left] <=(long long) (2LL* nums[right])) left++;
+            cnt+=(mid-left+1);
+            right++;
         }
         return cnt;
     }
-
     void mergeSort(int start, int mid, int end, vector<int>& nums) {
         int left = start, right = mid + 1;
         vector<int> temp;
@@ -32,17 +30,11 @@ public:
                 temp.emplace_back(nums[right++]);
             }
         }
-        while (left <= mid) {
-            temp.emplace_back(nums[left++]);
-        }
-        while (right <= end) {
-            temp.emplace_back(nums[right++]);
-        }
-        for (int i = start; i <= end; i++) {
+        while(left <= mid) temp.emplace_back(nums[left++]);
+        while(right <= end) temp.emplace_back(nums[right++]);
+        for (int i = start; i <= end; i++)
             nums[i] = temp[i - start];
-        }
     }
-
     int reversePairs(vector<int>& nums) {
         return merge(0, nums.size() - 1, nums);
     }
