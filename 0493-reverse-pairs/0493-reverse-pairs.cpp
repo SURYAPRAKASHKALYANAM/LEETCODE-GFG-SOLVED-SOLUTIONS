0cfbb0bb464b2404e85cb869247bb300d47f1fc9
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-07-26 22:35:43 +0530

    Time: 181 ms (93.79%), Space: 99.3 MB (93.73%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -7,7 +7,8 @@ public:
             cnt += merge(start, mid, nums);
             cnt += merge(mid + 1, end, nums);
             cnt += countPairs(start, mid, end, nums);
-            mergeSort(start, mid,end, nums);
+            // mergeSort(start, mid,end, nums);
+            inplace_merge(nums.begin()+start,nums.begin()+mid+1,nums.begin()+end+1);
         }
         return cnt;
     }
@@ -20,21 +21,21 @@ public:
         }
         return cnt;
     }
-    void mergeSort(int start, int mid, int end, vector<int>& nums) {
-        int left = start, right = mid + 1;
-        vector<int> temp;
-        while (left <= mid && right <= end) {
-            if (nums[left] <= nums[right]) {
-                temp.emplace_back(nums[left++]);
-            } else {
-                temp.emplace_back(nums[right++]);
-            }
-        }
-        while(left <= mid) temp.emplace_back(nums[left++]);
-        while(right <= end) temp.emplace_back(nums[right++]);
-        for (int i = start; i <= end; i++)
-            nums[i] = temp[i - start];
-    }
+    // void mergeSort(int start, int mid, int end, vector<int>& nums) {
+    //     int left = start, right = mid + 1;
+    //     vector<int> temp;
+    //     while (left <= mid && right <= end) {
+    //         if (nums[left] <= nums[right]) {
+    //             temp.emplace_back(nums[left++]);
+    //         } else {
+    //             temp.emplace_back(nums[right++]);
+    //         }
+    //     }
+    //     while(left <= mid) temp.emplace_back(nums[left++]);
+    //     while(right <= end) temp.emplace_back(nums[right++]);
+    //     for (int i = start; i <= end; i++)
+    //         nums[i] = temp[i - start];
+    // }
     int reversePairs(vector<int>& nums) {
         return merge(0, nums.size() - 1, nums);
     }
