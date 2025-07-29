5ef2a062e2aadcf0c28a23c1ff736500478c9a79
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-07-29 22:32:37 +0530

    Time: 1 ms (48.32%), Space: 10 MB (58.69%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAlnum(char c) {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
+               (c >= '0' && c <= '9');
+    }
+    bool isPalindrome(string s) {
+        int len = s.size();
+        int start = 0, end = len - 1;
+        while (start < end) {
+            while (start < end && !isAlnum(s[start]))
+                start++;
+            while (end > start && !isAlnum(s[end]))
+                end--;
+            if (start < end) {
+                if (tolower(s[start]) != tolower(s[end]))
+                    return false;
+                start++;
+                end--;
+            } else
+                return true;
+        }
+        return true;
+    }
+};
