cb0ed3bec28f11e9e048a139df5d0ca01c413913
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-08-03 13:31:22 +0530

    Time: 20 ms (40.98%), Space: 27.6 MB (22.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) 
-    {
-        vector<vector<string>> ans;
-        map<string,vector<string>> m;
-        for(auto i=0;i<strs.size();i++) 
-        {
-            auto k=strs[i];
-            sort(strs[i].begin(),strs[i].end());
-            m[strs[i]].emplace_back(k);
+    size_t array_hash(vector<int>& arr) {
+        size_t hashVal = 0;
+        const size_t multiplier = 31;
+
+        for (int x : arr) {
+            hashVal = hashVal * multiplier + hash<int>{}(x);
         }
-        
-        for(auto i=m.begin();i!=m.end();i++) ans.emplace_back(i->second);
+
+        return hashVal;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        map<size_t, vector<string>> m;
+        for (auto i = 0; i < strs.size(); i++) {
+            vector<int> charfreq(26, 0);
+            for (auto i : strs[i])
+                charfreq[i - 'a']++;
+            m[array_hash(charfreq)].emplace_back(strs[i]);
+        }
+
+        for (auto i = m.begin(); i != m.end(); i++)
+            ans.emplace_back(i->second);
         return ans;
-        
-        
     }
 };
