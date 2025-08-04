310648a34c924bfa1a9d4acd42a246aace6e1146
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-08-04 07:48:25 +0530

    Time: 68 ms (5.29%), Space: 27.4 MB (5.06%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -3,10 +3,11 @@ public:
     string multiply(string num1, string num2) {
         int end1 = num1.size() - 1;
         int end2 = num2.size() - 1;
-        int carry = 0;
+        int carry = 0, sum = 0;
         string res = "";
-        vector<string> products;
-        string Xs = "";
+        // vector<string> products;
+        // string Xs = "";
+        int startIdx = 1;
         int maxlen = 0;
         while (end1 >= 0) {
             string sub = "";
@@ -15,42 +16,45 @@ public:
                 int mul = (num1[end1] - '0') * (num2[end2] - '0');
                 mul += carry;
                 if (end2 != 0) {
-                    int rem = mul % 10;
-                    sub = sub + to_string(rem);
+                    sub = to_string(mul%10) + sub;
                     carry = mul / 10;
                 } else {
+                    if(mul==0) sub=to_string(0) + sub;
                     while (mul) {
-                        sub = sub + to_string(mul % 10);
+                        sub = to_string(mul % 10) + sub;
                         mul /= 10;
                     }
                 }
                 end2--;
             }
             end2 = num2.size() - 1;
-            sub = Xs + sub;
-            maxlen = max(maxlen, (int)sub.size());
-            products.push_back(sub);
-            Xs += "X";
-            end1--;
-        }
-        int sum = 0;
-        carry = 0;
-        for (int i = 0; i < maxlen; i++) {
-            sum = carry;
-            for (auto sub : products) {
-                if (sub.size() > i && sub[i] != 'X') {
-                    sum += (sub[i] - '0');
+            // cout<<res<<" "<<sub<<endl;
+            if (res == "")
+                res = sub;
+            else {
+                sum = 0, carry = 0;
+                int idx1 = res.size() - startIdx;
+                int idx2 = sub.size() - 1;
+                while (idx1 >= 0 && idx2 >= 0) {
+                    sum = carry + (res[idx1] - '0') + (sub[idx2] - '0');
+                    res[idx1] = '0' + (sum % 10);
+                    carry = sum / 10;
+                    idx1--;
+                    idx2--;
                 }
-            }
-            if (i == (maxlen - 1)) {
-                while (sum) {
+                while (idx2 >= 0) {
+                    sum = (sub[idx2] - '0') + carry;
                     res = to_string(sum % 10) + res;
-                    sum /= 10;
+                    carry = sum / 10;
+                    idx2--;
                 }
-            } else {
-                res = to_string(sum % 10) + res;
-                carry = sum / 10;
+                while (carry) {
+                    res = to_string(carry % 10) + res;
+                    carry = carry / 10;
+                }
             }
+            startIdx++;
+            end1--;
         }
         if (count(begin(res), end(res), '0') == res.size() || res.size() == 0)
             return "0";
