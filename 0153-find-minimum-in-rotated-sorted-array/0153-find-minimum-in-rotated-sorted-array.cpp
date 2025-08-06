b0779b0c1fa6a3f8a01467e5b33d3c92e06ebd48
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-08-06 05:54:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,23 +1,21 @@
-class Solution
-{
-    public:
-        int findMin(vector<int> &nums)
-        {
-            if(nums.size()==2) return min(nums[0],nums[1]);
-            if(nums[0]-nums.back() <0) return nums[0];
-            int mid, low = 0, high = nums.size() - 1;
-            while (low <= high)
-            {
-                if(high>0 && high <nums.size()-1)
-                if (nums[high - 1] > nums[high] && nums[high] < nums[high + 1]) return nums[high];
-                if(low>0 && low<nums.size()-1)
-                if (nums[low - 1] > nums[low] && nums[low] < nums[low + 1]) return nums[low];
-                mid = (low + high) / 2;
-                if (mid == 0 || mid == nums.size() - 1) return nums[mid];
-                if (nums[mid - 1] > nums[mid] && nums[mid] < nums[mid + 1]) return nums[mid];
-                else if (nums[low] - nums[mid] >= nums[mid] - nums[high]) high = mid-1;
-                else low = mid+1;
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int len = nums.size();
+        int start = 0 ,end = len-1,mid;
+        int minEle = 1e4;
+        while(start<=end){
+            mid = start + (end-start)/2;
+            // cout<<start<<" "<<mid<<" "<<end<<endl;
+            if(nums[mid]<=nums[end]){
+                minEle = nums[mid];
+                // cout<<minEle<<"\n";
+                end=mid-1;
+            } else{
+                // minEle = min(minEle,nums[mid+1]);
+                start=mid+1;
             }
-            return nums[mid];
         }
+        return minEle;
+    }
 };
