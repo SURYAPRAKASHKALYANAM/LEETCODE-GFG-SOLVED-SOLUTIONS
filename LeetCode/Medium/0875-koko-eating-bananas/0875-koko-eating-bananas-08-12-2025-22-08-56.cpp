34c74913a347e7516e931b16db3d276243d534a6
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-08-12 22:08:58 +0530

    3 ms (94.53%) 08-12-2025 0875-koko-eating-bananas .cpp

---
+++ LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas-08-12-2025-22-08-56.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long helper(vector<int>& piles, long long k, int lim) {
+        long long time = 0LL;
+        for (long long i : piles) {
+            time += (long long)((i / k) + (i % k ? 1 : 0));
+            if (time > lim)
+                return 2;
+        }
+        return (time == lim ? 1 : 0);
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int start = 1, end = *max_element(piles.begin(), piles.end());
+        int mid, ans = end;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            int val = helper(piles, mid, h);
+            if (val <= 1) {
+                ans = min(ans, mid);
+                end = mid - 1;
+            } else if (val == 2)
+                start = mid + 1;
+        }
+        return ans;
+    }
+};
