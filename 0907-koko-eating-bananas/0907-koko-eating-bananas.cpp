94a2c1b33c843fa714deb59283e92700620faaad
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-08-12 22:07:57 +0530

    Time: 3 ms (94.53%), Space: 23 MB (42.13%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    long long helper(vector<int>& piles, long long div) {
-        long long sum = 0LL;
+    long long helper(vector<int>& piles, long long k, int lim) {
+        long long time = 0LL;
         for (long long i : piles) {
-            sum += (long long)((i / div) + (i % div ? 1 : 0));
+            time += (long long)((i / k) + (i % k ? 1 : 0));
+            if (time > lim)
+                return 2;
         }
-        return sum;
+        return (time == lim ? 1 : 0);
     }
     int minEatingSpeed(vector<int>& piles, int h) {
         int start = 1, end = *max_element(piles.begin(), piles.end());
         int mid, ans = end;
         while (start <= end) {
             mid = start + (end - start) / 2;
-            if (helper(piles, mid) <= (long long)h) {
+            int val = helper(piles, mid, h);
+            if (val <= 1) {
                 ans = min(ans, mid);
                 end = mid - 1;
-            } else
+            } else if (val == 2)
                 start = mid + 1;
         }
         return ans;
