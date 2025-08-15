47e618f7c79874790c7835b8406d97abf71e5402
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-08-15 19:17:54 +0530

    Time: 0 ms (100%), Space: 8.1 MB (4.79%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n == 1)
-            return true;
-        if (n % 4 != 0)
+        if (n <= 0)
             return false;
-        while (n > 0) {
-            n = n / 4;
-            if (n == 1)
-                return true;
-            else if (n % 4 != 0)
-                return false;
-        }
-        return false;
+        return   ((n & (n - 1)) == 0) && ((int)log2(n)%2==0) ;
     }
 };
