04eba670de03d38fa1d9331ea2ff20e6a2721c6c
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-08-23 12:04:13 +0530

    Time: 0 ms (100%), Space: 10.5 MB (50.07%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,23 +1,25 @@
 class Solution {
-public:
-    void generate(int pos, vector<int>& nums, vector<int>& sub,
-                  set<vector<int>>& ans, int len) {
+   public:
+    void generate(vector<vector<int>>& ans, vector<int>& nums, int pos,
+                  vector<int>& sub, int len) {
         if (pos == len) {
-            ans.insert(sub);
+            ans.emplace_back(sub);
             return;
         }
         sub.emplace_back(nums[pos]);
-        generate(pos + 1, nums, sub, ans, len);
+        generate(ans, nums, pos + 1, sub, len);
         sub.pop_back();
-        generate(pos + 1, nums, sub, ans, len);
+        int nextPos = pos;
+        while (nextPos + 1 < len && nums[nextPos] == nums[nextPos + 1])
+            nextPos++;
+        generate(ans, nums, nextPos + 1, sub, len);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         // your code goes here
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<int> sub;
         sort(nums.begin(), nums.end());
-        generate(0, nums, sub, ans, nums.size());
-        vector<vector<int>> finalAns(ans.begin(), ans.end());
-        return finalAns;
+        generate(ans, nums, 0, sub, nums.size());
+        return ans;
     }
 };
