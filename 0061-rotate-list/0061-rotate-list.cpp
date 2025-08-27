b74d09418f91365c815bc6eaab97b6933291f384
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-08-27 22:17:11 +0530

    Time: 0 ms (100%), Space: 16.3 MB (93.71%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,32 +1,69 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
-class Solution
+/*
+Definition of singly linked list:
+struct ListNode
 {
-    public:
-        ListNode* rotateRight(ListNode *head, int k)
-        {
-            if(!head) return 0;
-            ListNode *temp = head, *end = head;
-            int len = 1;
-            while (temp->next)
-            {
-                temp = temp->next;
-                len++;
-            }
-            temp->next = head;
-            k = k % len;
-            k = len - k - 1;
-            while (k--) end = end->next;
-            head = end->next;
-            end->next = NULL;
-            return head;
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    int lenght(ListNode* node) {
+        int cnt = 0;
+        while (node) {
+            cnt++;
+            node = node->next;
         }
+        return cnt;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next)
+            return head;
+        ListNode* temp = head;
+        int len = lenght(head);
+        k = k % len;
+        while (k) {
+            temp = temp->next;
+            // if (!temp)
+            //     temp = head;
+            k--;
+        }
+        if (temp == head)
+            return head;
+        ListNode *newH, *start = head;
+        while (temp->next) {
+            start = start->next;
+            temp = temp->next;
+        }
+        newH = start->next;
+        start->next = nullptr;
+        temp->next = head;
+        return newH;
+    }
 };
