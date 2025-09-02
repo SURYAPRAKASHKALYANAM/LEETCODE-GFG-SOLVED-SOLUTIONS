a52f0991d781e4abc04ffaf69bac66cdab5744bd
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-02 07:55:17 +0530

    Time: 0 ms (100%), Space: 20.7 MB (26.18%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,26 +1,6 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(begin(nums), end(nums));
-        int len = nums.size();
-        int start = 0, end = len - 1, mid;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            // cout<<mid<<" ";
-            if ((mid - 1 >= 0 && nums[mid - 1] == nums[mid])) {
-                if ((mid - start + 1) % 2 == 0)
-                    start = mid + 1;
-                else
-                    end = mid - 2;
-            } else if ((mid + 1 < len && nums[mid] == nums[mid + 1])) {
-                if ((end - mid + 1) % 2 == 0)
-                    end = mid - 1;
-                else
-                    start = mid + 2;
-            } else {
-                return nums[mid];
-            }
-        }
-        return -1;
+        return accumulate(begin(nums), end(nums), 0, bit_xor<int>());
     }
 };
