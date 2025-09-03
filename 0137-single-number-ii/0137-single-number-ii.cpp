f9ea1399c03bc97441e613aed2fff552bba9f7eb
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-03 07:06:18 +0530

    Time: 0 ms (100%), Space: 13.2 MB (98.73%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -18,13 +18,21 @@ public:
         }
         return ans;
         */
-        /* O NLogN */
+        /* O NLogN
         int len = nums.size();
         sort(nums.begin(), nums.end());
         for (int i = 0; i < len - 3; i += 3) {
             if (nums[i] != nums[i + 1] && nums[i] != nums[i + 2])
                 return nums[i];
         }
-        return nums[len - 1];
+        return nums[len - 1]; */
+
+        // O N
+        int ones = 0, twos = 0;
+        for (int i : nums) {
+            ones = (ones ^ i) & ~twos;
+            twos = (twos ^ i) & ~ones;
+        }
+        return ones;
     }
 };
