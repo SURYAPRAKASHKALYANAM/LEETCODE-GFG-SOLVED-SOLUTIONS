2597878e7d14a56c59ce11fb3df9f33967ce33a7
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-03 06:47:10 +0530

    Time: 1 ms (47.19%), Space: 13.4 MB (66.03%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        /*
+        O (32*N);
         int bit = 1;
         int ans = 0;
         int cnt;
@@ -15,5 +17,14 @@ public:
             bit <<= 1;
         }
         return ans;
+        */
+        /* O NLogN */
+        int len = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < len - 3; i += 3) {
+            if (nums[i] != nums[i + 1] && nums[i] != nums[i + 2])
+                return nums[i];
+        }
+        return nums[len - 1];
     }
 };
