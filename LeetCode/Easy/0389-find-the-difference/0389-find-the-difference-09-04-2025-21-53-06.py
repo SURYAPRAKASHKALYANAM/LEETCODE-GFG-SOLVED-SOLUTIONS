5f10326d5e2bb487c77e56d34afab07dcf0423a2
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-04 21:53:06 +0530

    112 ms (5.04%) 09-04-2025 0389-find-the-difference .py

---
+++ LeetCode/Easy/0389-find-the-difference/0389-find-the-difference-09-04-2025-21-53-06.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s=list(s)
+        t=list(t)
+        for i in t:
+            if i in s:
+                s.remove(i)
+            else:
+                return i
+
+        
