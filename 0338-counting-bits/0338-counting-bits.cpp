eb30a2a44aaa14939ad86a7d75428ab975f719d2
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-04 22:13:38 +0530

    Time: 0 ms (100%), Space: 10.4 MB (96.99%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,25 +1,16 @@
-class Solution
-{
-    public:
-        vector<int> countBits(int n)
-        {
-            vector<int> ans(n + 1);
-            ans[0] = 0;
-            if(n==0) return ans;
-            ans[1] = 1;
-            int lastpow = 1, diff;
-            for (int i = 2; i <= n; i++)
-            {
-               	// optimal : ans[i]=ans[i>>1]+(i%2);
-                if ((i &(i - 1)) == 0)
-                {
-                    lastpow = i;
-                    ans[i] = 1;
-                    continue;
-                }
-                diff = i - lastpow;
-                ans[i] = ans[diff] + 1;
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1, 0);
+        int lastPow = 1;
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                lastPow = i;
+                ans[i] = 1;
+            } else {
+                ans[i] = ans[lastPow] + ans[i % lastPow];
             }
-            return ans;
         }
+        return ans;
+    }
 };
