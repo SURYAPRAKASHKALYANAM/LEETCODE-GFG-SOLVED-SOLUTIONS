e10042cee6c3ef015a0b65dd9fff0cac8ce94531
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-07 12:05:55 +0530

    Time: 36 ms (94.78%), Space: 95.5 MB (61.94%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
+        // optimal
+        int freq[1000002] = {0};
+        for (int i = 0, n = intervals.size(); i < n; ++i) {
+            ++freq[intervals[i][0]];
+            --freq[intervals[i][1] + 1];
+        }
+        int curr = 0, res = 1;
+        for (int i = 0; i < 1000002; ++i) {
+            curr += freq[i];
+            res = max(res, curr);
+        }
+        return res;
+        // extra space
         vector<int> start, ends;
         for (auto i : intervals) {
             start.emplace_back(i[0]);
