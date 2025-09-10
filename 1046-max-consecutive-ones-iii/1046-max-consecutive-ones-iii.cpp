a53d9efafbbd1a4adbce0a7b6d37937f059cbce0
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-10 08:35:38 +0530

    Time: 0 ms (100%), Space: 69.5 MB (73.19%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -3,6 +3,23 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         int n = nums.size();
         int maxLen = 0;
+        int l = 0, r = 0;
+        int zeros = 0;
+        while (r < n) {
+            if (nums[r] == 0) zeros++;
+            if (zeros <= k)
+                maxLen = max(r - l + 1, maxLen);
+            else {
+                if (nums[l] == 0) zeros--;
+                l++;
+            }
+            r++;
+        }
+        return maxLen;
+        /*
+        O(2N) BCZ MOVING LEFT AND RIGHT 
+        int n = nums.size();
+        int maxLen = 0;
         int l = 0, r = 0, cnt = 0;
         while (r < n) {
             if (nums[r] == 0)
@@ -15,5 +32,6 @@ public:
             r++;
         }
         return maxLen;
+        */
     }
 };
