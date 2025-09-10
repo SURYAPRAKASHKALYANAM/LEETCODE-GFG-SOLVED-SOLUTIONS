cb3697f96c2fed21ef11808d7dcd5fb99f933a8e
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-10 21:55:21 +0530

    Time: 0 ms (100%), Space: 10.2 MB (78.62%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    string reverseWords(string s) 
-    {
-        int start=s.size()-1,end=s.size()-1;
-        string ans="";
-        while(start>=0 || end>=0)
-        {
-            if(end<0 || start<0) break;
-            while(end>=0 && s[end]==' ') end--;
-            start=end;
-            while(start>=0 && s[start]!=' ') start--;
-            for(int i=start+1;i<=end&& s[i]!=' ';i++) ans+=s[i];
-            if(end-start>=1) ans+=' ';
-            end=start-1;
+    string reverseWords(string s) {
+        int n = s.size();
+        int start = n - 1, end;
+        string rev = "", sub = "";
+        while (start >= 0) {
+            while (start >= 0 && s[start] == ' ')
+                start--;
+            end = start;
+            sub = "";
+            while (start >= 0 && s[start] != ' ') {
+                sub = s[start] + sub;
+                start--;
+            }
+            if ((end - start) >= 1)
+                rev += sub + " ";
         }
-        ans.pop_back();
-       return ans; 
+        rev.pop_back();
+        return rev;
     }
 };
