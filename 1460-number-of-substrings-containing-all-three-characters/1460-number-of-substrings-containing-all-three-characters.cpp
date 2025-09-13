fc2a29ad833a4430b8124c96ba8c59a8f99c760d
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-14 00:23:56 +0530

    Time: 3 ms (89.45%), Space: 10.9 MB (80.25%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -2,23 +2,34 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         int len = s.size();
-        int hash[3] = {1, 1, 1};
-        int l = 0, r = 0;
-        int cnt = 0;
+        // OPTIMAL O(N)
+        int lastSeen[3] = {-1, -1, -1};
         int ans = 0;
-        while (r < len) {
-            if (hash[s[r] - 'a'] > 0)
-                cnt++;
-            hash[s[r] - 'a']--;
-            while (cnt >= 3) {
-                hash[s[l] - 'a']++;
-                if (hash[s[l] - 'a'] > 0)
-                    cnt--;
-                l++;
+        for (int i = 0; i < len; i++) {
+            lastSeen[s[i] - 'a'] = i;
+            if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
+                ans += 1 + (*min_element(lastSeen, lastSeen + 3));
             }
-            ans += (l);
-            r++;
         }
         return ans;
+        // O (2*N)  SPACE
+        // int hash[3] = {1, 1, 1};
+        // int l = 0, r = 0;
+        // int cnt = 0;
+        // int ans = 0;
+        // while (r < len) {
+        //     if (hash[s[r] - 'a'] > 0)
+        //         cnt++;
+        //     hash[s[r] - 'a']--;
+        //     while (cnt >= 3) {
+        //         hash[s[l] - 'a']++;
+        //         if (hash[s[l] - 'a'] > 0)
+        //             cnt--;
+        //         l++;
+        //     }
+        //     ans += (l);
+        //     r++;
+        // }
+        // return ans;
     }
 };
