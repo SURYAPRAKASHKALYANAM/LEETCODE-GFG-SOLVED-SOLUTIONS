c6b54aa5d86a22a78d48367b56773e405ae7b967
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-15 22:27:43 +0530

    2 ms (34.86%) 09-15-2025 0338-counting-bits .cpp

---
+++ LeetCode/Easy/0338-counting-bits/0338-counting-bits-09-15-2025-22-27-42.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1, 0);
+        int lastPow;
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                lastPow = i;
+                ans[i] = 1;
+            } else {
+                ans[i] = 1 + ans[i % lastPow];
+            }
+        }
+        return ans;
+    }
+};
