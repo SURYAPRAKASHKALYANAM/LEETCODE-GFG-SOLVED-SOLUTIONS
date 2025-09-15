f5ac90f49c2536b5e3059df8682cb02f728cce30
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-15 20:32:31 +0530

    Time: 0 ms (100%), Space: 21.5 MB (96.54%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,42 +1,22 @@
-class Solution
-{
-    public:
-        vector<int> asteroidCollision(vector<int> &asteroids)
-        {
-            vector<int> st;
-            for (auto i: asteroids)
-            {
-                if (i > 0)
-                {
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+        for (int i : asteroids) {
+            if (i > 0 || st.empty() || st.back() < 0) {
+                st.push_back(i);
+            } else {
+                while (!st.empty() && st.back() > 0 && st.back() < abs(i))
+                    st.pop_back();
+                if (!st.empty()) {
+                    if (st.back() > 0 && st.back() == abs(i))
+                        st.pop_back();
+                    else if (st.back() < 0)
+                        st.push_back(i);
+                } else
                     st.push_back(i);
-                    continue;
-                }
-                if (!st.empty() && st.back() < 0)
-                {
-                    st.push_back(i);
-                    continue;
-                }
-                else
-                {
-                    st.push_back(i);
-                    int pos = st.size() - 1;
-                    while ((pos - 1) >= 0 && st[pos]<0 && st[pos - 1]>0)
-                    {
-                        if (abs(st[pos]) < abs(st[pos - 1]))
-                        {
-                            st.pop_back();
-                            pos--;
-                        }
-                        else if (abs(st[pos]) > abs(st[pos - 1]))
-                        {
-                            st[pos - 1] = st[pos];
-                            pos--;
-                            st.pop_back();
-                        }
-                        else pos -= 2, st.pop_back(), st.pop_back();
-                    }
-                }
             }
-            return st;
         }
+        return st;
+    }
 };
