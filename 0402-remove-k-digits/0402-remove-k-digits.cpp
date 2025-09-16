5081b86fba08c69251877ff4b7a2a6ff38cb5097
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-16 23:00:00 +0530

    Time: 8 ms (14.43%), Space: 11.2 MB (85.43%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string st = "";
+        for (auto i : num) {
+            if (st.empty() && i != '0') {
+                st.push_back(i);
+            } else {
+                while (!st.empty() && st.back() > i && k > 0) {
+                    k--;
+                    st.pop_back();
+                }
+                if (st.empty() && i == '0')
+                    continue;
+                st.push_back(i);
+            }
+        }
+        while (k && !st.empty()) {
+            k--;
+            st.pop_back();
+        }
+        // string s = "";
+        // while (!st.empty()) {
+        //     s = st.top() + s;
+        //     st.pop();
+        // }
+        return st == "" ? "0" : st;
+    }
+};
