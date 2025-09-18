93a1495f23cd04761f9122f61eca58a640f5f854
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-09-18 21:45:44 +0530

    Time: 28 ms (42.09%), Space: 139.1 MB (59.36%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,26 +1,18 @@
-class Solution
-{
-    public:
-        vector<int> maxSlidingWindow(vector<int> &nums, int k)
-        {
-            priority_queue<pair<int, int>> q;
-            for (int i = 0; i < k; i++) q.push({nums[i],i});
-            vector<int> ans;
-            int ele = q.top().first, index = q.top().second;
-            ans.emplace_back(ele);
-            int left = 0, right = k;
-            while (right < nums.size())
-            {
-                left++;
-                q.push({nums[right],right++});
-                ele = q.top().first, index = q.top().second;
-                while (!q.empty() && index < left) 
-                {
-                    q.pop();
-                    ele = q.top().first, index = q.top().second;
-                }
-                ans.emplace_back(ele);
-            }
-            return ans;
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        int len = nums.size();
+        deque<int> dq;
+        for (int i = 0; i < len; i++) {
+            while (!dq.empty() && dq.front() <= i - k)
+                dq.pop_front();
+            while (!dq.empty() && nums[dq.back()] <= nums[i])
+                dq.pop_back();
+            dq.push_back(i);
+            if (i + 1 - k >= 0)
+                ans.emplace_back(nums[dq.front()]);
         }
+        return ans;
+    }
 };
