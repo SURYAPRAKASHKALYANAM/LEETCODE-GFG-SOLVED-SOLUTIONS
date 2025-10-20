8a2e4c09540eec1157d17d8fa75a807e7b809212
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-10-20 05:48:50 +0530

    Time: 0 ms (100%), Space: 17.7 MB (42.92%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for (auto i : operations) {
+            if (i[0] == '-' || i[2] == '-')
+                x--;
+            else if (i[0] == '+' || i[2] == '+')
+                x++;
+        }
+        return x;
+    }
+};
