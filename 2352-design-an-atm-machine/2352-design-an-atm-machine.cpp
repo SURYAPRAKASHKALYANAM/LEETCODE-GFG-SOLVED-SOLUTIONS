1333e2d98463bf0224f64e5ea6fbc301738a87fc
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-10-29 06:02:04 +0530

    Time: 3 ms (98.73%), Space: 109.4 MB (93.01%) - LeetHub

--- 2352-design-an-atm-machine/2352-design-an-atm-machine.cpp
+++ 2352-design-an-atm-machine/2352-design-an-atm-machine.cpp
@@ -1,30 +1,29 @@
 class ATM {
 public:
     vector<int> denominations = {20, 50, 100, 200, 500};
-    vector<long long> balance;
+    vector<int> balance;
     ATM() { balance.resize(5, 0); }
 
     void deposit(vector<int> banknotesCount) {
         for (int i = 0; i < 5; i++)
-            balance[i] = (long long) (balance[i]+banknotesCount[i]);
+            balance[i] = (balance[i] + banknotesCount[i]);
     }
 
     vector<int> withdraw(int amount) {
         vector<int> notes(5, 0);
-        vector<long long> balanceDummy(balance.begin(), balance.end());
         for (int i = 4; i >= 0; i--) {
             if (balance[i] > 0 && denominations[i] <= amount) {
-                long long avail = balance[i];
-                long long possible = (long long)amount / (long long)denominations[i];
-                long long withdraw = min(avail, possible);
-                balance[i] -= withdraw;
-                amount -= ((long long)withdraw * (long long)denominations[i]);
-                notes[i] = withdraw;
+                int possible = amount / denominations[i];
+                notes[i] = min(balance[i], possible);
+                amount -= (notes[i] * denominations[i]);
             }
         }
-        if (amount != 0)
-            balanceDummy.swap(balance);
-        return (amount != 0) ? vector<int>(1, -1) : notes;
+        if (amount != 0) {
+            return {-1};
+        }
+        for (int i = 0; i < 5; i++)
+            balance[i] -= notes[i];
+        return notes;
     }
 };
 
