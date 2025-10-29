106c2404d85f18961a705dd5d9dbf1b52b23ceaf
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-10-29 06:27:11 +0530

    Time: 10 ms (45.79%), Space: 24.5 MB (57.02%) - LeetHub

--- 1169-largest-values-from-labels/1169-largest-values-from-labels.cpp
+++ 1169-largest-values-from-labels/1169-largest-values-from-labels.cpp
@@ -4,20 +4,34 @@ public:
                               int numWanted, int useLimit) {
         unordered_map<int, int> labelsUsed;
         int len = labels.size(), sum = 0, cnt = 0;
-        for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j++) {
-                if (values[j] > values[i]) {
-                    swap(values[j], values[i]);
-                    swap(labels[j], labels[i]);
-                }
+        // O(N^2)
+        // for (int i = 0; i < len; i++) {
+        //     for (int j = i + 1; j < len; j++) {
+        //         if (values[j] > values[i]) {
+        //             swap(values[j], values[i]);
+        //             swap(labels[j], labels[i]);
+        //         }
+        //     }
+        //     if (labelsUsed[labels[i]] < useLimit) {
+        //         labelsUsed[labels[i]]++;
+        //         sum += values[i];
+        //         cnt++;
+        //     }
+        //     if (cnt == numWanted)
+        //         break;
+        // }
+        // priority queue
+        priority_queue<pair<int, int>> pq;
+        for (int i = 0; i < len; i++)
+            pq.push({values[i], labels[i]});
+        while (!pq.empty() && numWanted) {
+            auto it = pq.top();
+            pq.pop();
+            if (labelsUsed[it.second] < useLimit) {
+                sum += it.first;
+                numWanted--;
+                labelsUsed[it.second]++;
             }
-            if (labelsUsed[labels[i]] < useLimit) {
-                labelsUsed[labels[i]]++;
-                sum += values[i];
-                cnt++;
-            }
-            if (cnt == numWanted)
-                break;
         }
         return sum;
     }
