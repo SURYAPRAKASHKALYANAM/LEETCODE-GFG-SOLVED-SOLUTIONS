21a77507b3f9d999dd092fe5e79298a06edb249c
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-12-15 09:17:53 +0530

    Time: 0 ms (100%), Space: 107.1 MB (24.91%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -3,11 +3,16 @@ public:
     long long getDescentPeriods(vector<int>& prices) {
         long long ans = 0;
         int len = prices.size();
-        vector<int> dp(len, 1);
+        // vector<int> dp(len, 1);
+        int prev = 1;
         for (int i = 0; i < len; i++) {
+            // dp[i] = dp[i - 1] + 1;
             if (i - 1 >= 0 && prices[i - 1] == (prices[i] + 1))
-                dp[i] = dp[i - 1] + 1;
-            ans += (1LL * dp[i]);
+                prev++;
+            else
+                prev = 1;
+            // ans += (1LL * dp[i]);
+            ans += (1LL * prev);
         }
         return ans;
     }
