4608c9d260663b8ff58056c8bd76b5ac121facc8
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-12-25 05:54:29 +0530

    Time: 177 ms (29.43%), Space: 111.9 MB (12.06%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        long long ans=0;
-        priority_queue<int> q(happiness.begin(),happiness.end());
-        int curr=0;
-        while(k--)
-        {
-            if(q.top()-curr>0) ans+=(q.top()-curr);
-            else break;
+        long long ans = 0;
+        priority_queue<int> q(happiness.begin(), happiness.end());
+        int curr = 0;
+        while (k--) {
+            if (q.top() - curr > 0)
+                ans += (q.top() - curr);
+            else
+                break;
             curr++;
             q.pop();
         }
