30bb305076f5d2362f66489b1e8dbd723d32dab4
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2025-12-30 06:19:14 +0530

    Time: 0 ms (100%), Space: 9.3 MB (50.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,26 @@
-class Solution
-{
-    public:
-       	// recursive is not otimal solution it takes more time
-       	// int find(int i,int j,int m,int n)
-       	// {
-       	//     if(i>=m && j!=n-1) return 0;
-       	//     if(j>=n && i!=m-1) return 0;
-       	//     if(i==m-1 && j==n-1) return 1;
-       	//     return find(i+1,j,m,n)+find(i,j+1,m,n);
-       	// }
-        int uniquePaths(int m, int n)
-        {
-           	//  use combinatrics
-            double res = 1;
-            int d = m + n - 2;
-            int r = m - 1;
-            for (int i = 1; i <= r; i++)
-            {
-                res = res *(d - r + i) / i;
-            }
-            return (int) res;
-        }
+class Solution {
+public:
+    int helper(int m, int n, vector<vector<int>>& dp) {
+        if (m < 0 || n < 0)
+            return 0;
+        if (m == 0 && n == 0)
+            return 1;
+        if (dp[m][n] != -1)
+            return dp[m][n];
+        return dp[m][n] = helper(m - 1, n, dp) + helper(m, n - 1, dp);
+    }
+    int uniquePaths(int m, int n) {
+        //  use combinatrics
+        // double res = 1;
+        // int d = m + n - 2;
+        // int r = m - 1;
+        // for (int i = 1; i <= r; i++)
+        // {
+        //     res = res *(d - r + i) / i;
+        // }
+        // return (int) res;
+        // dp
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(m - 1, n - 1, dp);
+    }
 };
