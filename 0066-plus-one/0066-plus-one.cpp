b06e878dc16ea275f468f01890c128c4569aec29
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-01-01 05:42:32 +0530

    Time: 0 ms (100%), Space: 11.6 MB (14.65%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int len = digits.size() - 1;
+        int sum = 0, carry = 1;
+        while (carry && len >= 0) {
+            digits[len] += carry;
+            carry = digits[len] / 10;
+            digits[len] %= 10;
+            len--;
+        }
+        if (carry)
+            digits.insert(digits.begin(), carry);
+        return digits;
+    }
+};
