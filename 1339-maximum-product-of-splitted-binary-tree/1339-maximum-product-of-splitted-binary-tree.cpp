6348b30df16aab6a7cd094b722126806f9222171
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-01-07 07:03:53 +0530

    Time: 4 ms (65.02%), Space: 78 MB (73.71%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -15,6 +15,13 @@ public:
     int mod = 1e9 + 7;
     long long totalSum = 0;
     long long maxProd = 0;
+    void addAll(TreeNode* root) {
+        if (!root)
+            return;
+        totalSum += (root->val);
+        addAll(root->left);
+        addAll(root->right);
+    }
     long long helper(TreeNode* root) {
         if (!root)
             return 0;
@@ -25,7 +32,7 @@ public:
         return root->val + left + right;
     }
     int maxProduct(TreeNode* root) {
-        totalSum = helper(root);
+        addAll(root);
         helper(root);
         return maxProd % mod;
     }
