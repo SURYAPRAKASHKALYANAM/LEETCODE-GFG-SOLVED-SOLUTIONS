2af57f7cf78b9ee30cdddfb27cb1f57b1f5a4e26
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-01-16 21:58:01 +0530

    N/A (0%) 01-16-2026 2975-maximum-square-area-by-removing-fences-from-a-field .cpp

---
+++ LeetCode/Medium/2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field-01-16-2026-21-58-02.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maximizeSquareArea(int m, int n, vector<int>& hFences,
+                           vector<int>& vFences) {
+        hFences.emplace_back(1);
+        vFences.emplace_back(1);
+        sort(hFences.begin(), hFences.end());
+        sort(vFences.begin(), vFences.end());
+        hFences.emplace_back(m);
+        vFences.emplace_back(n);
+        int hLen = hFences.size(), vLen = vFences.size();
+        unordered_map<long long, int> possibleLen;
+        long long max_area = 0, mod = 1e9 + 7;
+        for (int i = 0; i < hLen; i++) {
+            for (int j = i; j < hLen; j++) {
+                long len;
+                if (i == j)
+                    len = hFences[i] - 1;
+                else
+                    len = hFences[j] - hFences[i];
+                // cout << len << " ";
+                possibleLen[len]++;
+            }
+        }
+        for (int i = 0; i < vLen; i++) {
+            for (int j = i; j < vLen; j++) {
+                long len;
+                if (i == j)
+                    len = vFences[i] - 1;
+                else
+                    len = vFences[j] - vFences[i];
+                // cout << len << " ";
+                if (possibleLen[len] != 0) {
+                    max_area = max((len * len % mod), max_area);
+                }
+            }
+        }
+
+        return max_area == 0 ? -1 : max_area;
+    }
+};
