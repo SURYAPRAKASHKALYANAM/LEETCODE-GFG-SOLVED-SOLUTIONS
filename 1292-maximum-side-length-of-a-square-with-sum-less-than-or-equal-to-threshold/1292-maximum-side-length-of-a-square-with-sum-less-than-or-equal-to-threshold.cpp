18254364b60f00aa28b0b533692be2382508e5fd
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-01-20 00:58:59 +0530

    Time: 160 ms (12.73%), Space: 32.9 MB (12.27%) - LeetHub

---
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    long sum(vector<vector<int>>& pref, int i, int j, int k) {
+        long total = 0;
+        for (int r = i; r < (i + k); r++) {
+            if (j > 0)
+                total += (pref[r][j + k - 1] - pref[r][j - 1]);
+            else
+                total += (pref[r][j + k - 1]);
+        }
+        return total;
+    }
+    int maxSideLength(vector<vector<int>>& mat, int threshold) {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> pref(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j > 0)
+                    pref[i][j] = pref[i][j - 1] + mat[i][j];
+                else
+                    pref[i][j] = mat[i][j];
+            }
+        }
+        int poss = min(m, n), max_len = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int start = 1, end = poss, k;
+                while (start <= end) {
+                    k = (start + end) / 2;
+                    if (k < max_len) {
+                        start = k + 1;
+                        continue;
+                    }
+                    if ((i + k) <= m && (j + k) <= n) {
+                        int tot = sum(pref, i, j, k);
+                        if (tot <= threshold) {
+                            max_len = max(max_len, k);
+                            start = (k + 1);
+                        } else
+                            end = k - 1;
+                    } else
+                        end = k - 1;
+                }
+            }
+        }
+        return max_len;
+    }
+};
