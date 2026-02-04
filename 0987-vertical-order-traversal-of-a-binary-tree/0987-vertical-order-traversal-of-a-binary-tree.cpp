cd98de109adbd16cbfebb4a2565d3200a6f17554
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-02-04 06:08:46 +0530

    Time: 3 ms (40.38%), Space: 16.4 MB (42.55%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -6,35 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int, map<int, multiset<int>>> nodes;
+        queue<pair<TreeNode*, pair<int, int>>> q;
         vector<vector<int>> ans;
-        map<int,map<int,multiset<int>>> nodes;
-        queue<pair<TreeNode*,pair<int,int>>> todo;
-        todo.push({root,make_pair(0,0)});
-        while(!todo.empty())
-        {
-            auto it=todo.front();
-            todo.pop();
-            int x=it.second.first,y=it.second.second;
+        q.push({root, {0, 0}});
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            int x = it.second.first, y = it.second.second;
             nodes[x][y].insert(it.first->val);
-            if(it.first->left) todo.push({it.first->left,{x-1,y+1}});
-            if(it.first->right) todo.push({it.first->right,{x+1,y+1}});
-        }
-        for(auto p:nodes)
-        {
-            vector<int> col;
-            for(auto lev:p.second)
-            {
-                col.insert(col.end(),lev.second.begin(),lev.second.end());
+            if (it.first->left) {
+                q.push({it.first->left, {x - 1, y + 1}});
             }
-            ans.emplace_back(col);
+            if (it.first->right) {
+                q.push({it.first->right, {x + 1, y + 1}});
+            }
+        }
+        for (auto [xInd, order] : nodes) {
+            vector<int> temp;
+            for (auto [yInd, suborder] : order) {
+                temp.insert(temp.end(), suborder.begin(), suborder.end());
+            }
+            ans.emplace_back(temp);
         }
         return ans;
-        
     }
 };
