fc63e204326d3815782c75d243ee013d185ca98d
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-02-04 05:35:16 +0530

    Time: 0 ms (100%), Space: 23.7 MB (42.42%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -6,24 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int dia=-1;
-    int maxdepth(TreeNode* root)
-    {
-        if(!root) return 0;
-        int lh=maxdepth(root->left);
-        int rh=maxdepth(root->right);
-        dia=max(lh+rh,dia);
-        return max(lh,rh)+1;
+    int helper(TreeNode* root, int& max_dia) {
+        if (!root)
+            return 0;
+        int lh = helper(root->left, max_dia);
+        int rh = helper(root->right, max_dia);
+        max_dia = max(max_dia, lh + rh);
+        return 1 + max(lh, rh);
     }
-    int diameterOfBinaryTree(TreeNode* root) 
-    {
-        maxdepth(root);
-        return dia;
-        
+    int diameterOfBinaryTree(TreeNode* root) {
+        int max_dia = 0;
+        helper(root, max_dia);
+        return max_dia;
     }
 };
