302a496f05900a13d13469430bdf312183374e8a
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-02-07 18:34:32 +0530

    Time: 2 ms (5.46%), Space: 13.1 MB (10.18%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -6,61 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void find(TreeNode *root,vector<int>& s,vector<vector<int>>& ans)
-    {
-        if(root==NULL) 
-        {
-            ans.emplace_back(s);
+    void find(TreeNode* root, string s, int& totSum) {
+        if (!root)
             return;
+        s += to_string(root->val);
+        if (!root->left && !root->right) {
+            totSum += stoi(s);
         }
-        if(!(root->left) && !(root->right))
-        {
-            s.emplace_back(root->val);
-            ans.emplace_back(s);
-            s.pop_back();
-            return;
-        }
-        else if(!(root->left))
-        {
-            s.emplace_back(root->val);
-            find(root->right,s,ans);
-            s.pop_back();
-        }
-        else if(!(root->right))
-        {
-             s.emplace_back(root->val);
-             find(root->left,s,ans);
-             s.pop_back();
-        }
-        else
-        {
-            s.emplace_back(root->val);
-            find(root->left,s,ans);
-            find(root->right,s,ans);
-            s.pop_back();
-        }  
+        find(root->left, s, totSum);
+        find(root->right, s, totSum);
     }
-    int sumNumbers(TreeNode* root) 
-    {
-        vector<vector<int>> ans;
-        vector<int> sub;
-        find(root,sub,ans);
-        int anss=0,s=0;
-        for(auto i:ans)
-        {
-            for(int j=0;j<i.size();j++) 
-            {
-                s+=(i[j]*pow(10,i.size()-j-1));
-            }
-            anss+=s;
-            s=0;
-        }
-        return anss;
-        
+    int sumNumbers(TreeNode* root) {
+        string s;
+        int totSum = 0;
+        find(root, s, totSum);
+        return totSum;
     }
 };
