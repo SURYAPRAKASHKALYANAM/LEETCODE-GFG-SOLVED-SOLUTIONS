e1ab80f1ed192bae2f47c7f2b1838630a389f86a
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-02-18 05:42:25 +0530

    Time: 0 ms (100%), Space: 7.9 MB (30.66%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,33 +1,14 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        bool flag=0,oddf=0,cf=0;
-        if(n&1)
-        {
-            oddf=1;
-            flag=1;
-            cf=1;
-        }
-        while(n>=1)
-        {
-            if(oddf)
-            {
-                if((n%2)==cf)
-                {
-                    cf=!cf;
-                    n=n/2;
-                }
-                else return false;
-            }
-            else
-            {
-                if((n%2)==cf)
-                {
-                    cf=!cf;
-                    n=n/2;
-                }
-                else return false;
-            }
+        bool needOne = (n & 1);
+        while (n) {
+            if (needOne && !(n & 1))
+                return false;
+            else if (!needOne && n & 1)
+                return false;
+            n >>= 1;
+            needOne = !needOne;
         }
         return true;
     }
