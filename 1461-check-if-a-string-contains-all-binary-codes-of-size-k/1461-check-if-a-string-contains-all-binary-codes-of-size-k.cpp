7de7423227af137ddb3d5624cea3892695c53eca
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-02-23 06:17:40 +0530

    Time: 405 ms (59.96%), Space: 119.1 MB (52.17%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -6,10 +6,10 @@ public:
             return false;
         unordered_set<string> st;
         for (int i = 0; i <= (len - k); i++) {
-            string sub = "";
-            for (int j = i; j < i + k; j++)
-                sub += s[j];
-            st.insert(sub);
+            // string sub = "";
+            // for (int j = i; j < i + k; j++)
+            //     sub += s[j];
+            st.insert(s.substr(i, k));
         }
         return st.size() == (1 << k);
     }
