5b1e3e625c8ecc55de3c95eee742f245e3a5bfce
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-03-01 06:14:20 +0530

    Time: 0 ms (100%), Space: 16.4 MB (81.74%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,13 +1,6 @@
-class Solution
-{
-    public:
-        int minPartitions(string s)
-        {
-            int ans = 0;
-            for (auto i = 0; i < s.size(); i++)
-            {
-                ans = max(ans, s[i] - '0');
-            }
-            return ans;
-        }
+class Solution {
+public:
+    int minPartitions(string n) {
+        return *max_element(n.begin(), n.end()) - '0';
+    }
 };
