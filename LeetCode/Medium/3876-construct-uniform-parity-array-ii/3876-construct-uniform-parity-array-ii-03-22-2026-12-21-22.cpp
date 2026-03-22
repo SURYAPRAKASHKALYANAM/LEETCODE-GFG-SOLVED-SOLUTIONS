927a840a9f3ce15f4dd14508945f3045a4ffc31d
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-03-22 12:21:22 +0530

    N/A (0%) 03-22-2026 3876-construct-uniform-parity-array-ii .cpp

---
+++ LeetCode/Medium/3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii-03-22-2026-12-21-22.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool allSame(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            if ((nums[i] & 1) != (nums[i - 1] & 1))
+                return false;
+        }
+        return true;
+    }
+    bool isPoss(vector<int>& nums, int parity, int mini) {
+        for (int i : nums) {
+            if ((i & 1) == parity)
+                continue;
+            if (i - mini < 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool uniformArray(vector<int>& nums1) {
+        if (allSame(nums1))
+            return true;
+        int len = nums1.size();
+        int omin = INT_MAX;
+        for (int& i : nums1) {
+            if (i & 1) {
+                omin = min(omin, i);
+        }
+        return isPoss(nums1, 0, omin) || isPoss(nums1, 1, omin);
+    }
+};
