6fca7beb87c09b6846ce62526ef29e3029a7f4aa
SURYA PRAKASH KALYANAM <115471106+SURYAPRAKASHKALYANAM@users.noreply.github.com>
2026-05-22 07:24:38 +0530

    Time: 23 ms (19.65%), Space: 23.6 MB (18.04%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -34,8 +34,8 @@ public:
         return max_score;
     }
 
-    int maxScoreWords(vector<string>& words, vector<char>& letters,
-                      vector<int>& score) {
+    int backTrack(vector<string>& words, vector<char>& letters,
+                  vector<int>& score) {
         vector<int> charCnt(26, 0);
         for (char ch : letters) {
             charCnt[ch - 'a']++;
@@ -54,5 +54,51 @@ public:
         }
 
         return find(0, wordCounts, wordScores, charCnt);
+    }
+
+    int optimal(vector<string>& words, vector<char>& letters,
+                vector<int>& score) {
+        // bit masking
+        int n = words.size();
+        vector<int> availableChars(26, 0);
+        for (char ch : letters)
+            availableChars[ch - 'a']++;
+        vector<vector<int>> wordChars(n, vector<int>(26, 0));
+        vector<int> wordScore(n, 0);
+        for (int i = 0; i < n; i++) {
+            for (char ch : words[i]) {
+                wordChars[i][ch - 'a']++;
+                wordScore[i] += score[ch - 'a'];
+            }
+        }
+        int maxScore = 0;
+        int totalSubs = (1 << n);
+        for (int mask = 1; mask < totalSubs; mask++) {
+            int currScore = 0;
+            bool isValid = true;
+            vector<int> currChars = availableChars;
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    currScore += wordScore[i];
+                    for (int j = 0; j < 26; j++) {
+                        currChars[j] -= wordChars[i][j];
+                        if (currChars[j] < 0) {
+                            isValid = false;
+                            break;
+                        }
+                    }
+                }
+                if (!isValid)
+                    break;
+            }
+            if (isValid)
+                maxScore = max(maxScore, currScore);
+        }
+        return maxScore;
+    }
+    int maxScoreWords(vector<string>& words, vector<char>& letters,
+                      vector<int>& score) {
+        return optimal(words, letters, score);
+        return backTrack(words, letters, score);
     }
 };
